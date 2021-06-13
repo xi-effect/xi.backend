dfadb211d37155bc2a476face1bf1d13b7d51ef8
qwert45hi <qwert45hi@yandex.ru>
2021-06-14 02:21:47 +0300

    0.7.6c

--- api_resources/outside/updater.py
+++ api_resources/outside/updater.py
@@ -1,23 +1,26 @@
 from api_resources.base.checkers import argument_parser
 from api_resources.base.discorder import send_discord_message, WebhookURLs
+from api_resources.base.anywherer import execute_in_console, disable_webapp
 
 from flask_restful import Resource
 from flask_restful.reqparse import RequestParser
-from json import loads
+
+github_token: str = ""
 
 
 class GithubWebhook(Resource):  # /update/
     parser: RequestParser = RequestParser()
-    parser.add_argument("commits", str)
     parser.add_argument("X-GitHub-Event", str, location="headers")
 
-    @argument_parser(parser, ("X-GitHub-Event", "event_type"), "commits")
-    def post(self, event_type: str, commits: str):
+    @argument_parser(parser, ("X-GitHub-Event", "event_type"))
+    def post(self, event_type: str):
         if event_type == "push":
-            version = loads(commits)["message"]
-            send_discord_message(WebhookURLs.STATUS, f"New API version {version} uploaded!")
+            send_discord_message(WebhookURLs.GITHUB, f"Got a push notification.\n"
+                                                     f"Starting auto-update")
+            execute_in_console("python updater.py")
+            disable_webapp()
         elif event_type == "release":
-            send_discord_message(WebhookURLs.GITHUB, f"Got a {event_type} notification.\n"
+            send_discord_message(WebhookURLs.GITHUB, f"Got a release notification.\n"
                                                      f"Releases are not supported yet!")
         else:
             send_discord_message(WebhookURLs.GITHUB, f"Got a {event_type} notification.\n"
