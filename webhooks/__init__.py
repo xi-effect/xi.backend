c223952f6e960d4e0c63590dbf0c6e2553ff3e30
qwert45hi <qwert45hi@yandex.ru>
2021-07-03 16:54:36 +0300

    0.7.7d: Error Report System Fix 2

--- webhooks/__init__.py
+++ webhooks/__init__.py
@@ -1,4 +1,4 @@
 from webhooks.discorder import send_message as send_discord_message, WebhookURLs
-from webhooks.discorder import send_long_message as send_long_discord_message
+from webhooks.discorder import send_file_message as send_file_discord_message
 from webhooks.anywherer import execute_in_console, execute_script_in_console
 from webhooks.anywherer import webapp_action, enable_webapp, disable_webapp, reload_webapp
