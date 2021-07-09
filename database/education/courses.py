e744a657dcd0b0e5777e4c81055ce06cdc8cbbd9
qwert45hi <qwert45hi@yandex.ru>
2021-07-10 02:41:19 +0300

    The Renascence of Modules

--- database/education/courses.py
+++ database/education/courses.py
@@ -1,42 +1,41 @@
 from datetime import datetime
+from enum import Enum
 from pickle import dumps, loads
 from random import randint
-from typing import Dict, List, Set
+from typing import Dict, List
 
 from flask_sqlalchemy import BaseQuery
 
 from database.base.basic import Identifiable
-from database.education.sessions import CourseFilterSession
+from database.education.sessions import ModuleFilterSession
 from main import db
 
 
 class Point(db.Model):
     __tablename__ = "points"
 
-    course_id = db.Column(db.Integer, primary_key=True)
     module_id = db.Column(db.Integer, primary_key=True)
     point_id = db.Column(db.Integer, primary_key=True)
 
     type = db.Column(db.Integer, nullable=False)  # 0 - Theory; 1 - HyperBlueprint
-    data = db.Column(db.PickleType, nullable=False)  # List[int] (all used page ids)
+    data = db.Column(db.PickleType, nullable=False)  # do a relation!
 
     @classmethod
-    def __create(cls, course_id: int, module_id: int, point_id: int, point_type: int, data: List[int]):
-        if cls.find_by_ids(course_id, module_id, point_id):
+    def __create(cls, module_id: int, point_id: int, point_type: int, data: List[int]):
+        if cls.find_by_ids(module_id, point_id):
             return False
-        new_point = cls(course_id=course_id, module_id=module_id, point_id=point_id,
-                        type=point_type, data=dumps(data))
+        new_point = cls(module_id=module_id, point_id=point_id, type=point_type, data=dumps(data))
         db.session.add(new_point)
         db.session.commit()
         return True
 
     @classmethod
-    def find_by_ids(cls, course_id: int, module_id: int, point_id: int):
-        return cls.query.filter_by(course_id=course_id, module_id=module_id, point_id=point_id).first()
+    def find_by_ids(cls, module_id: int, point_id: int):
+        return cls.query.filter_by(module_id=module_id, point_id=point_id).first()
 
     @classmethod
-    def get_module_points(cls, course_id: int, module_id: int):
-        return cls.query.filter_by(course_id=course_id, module_id=module_id).all()
+    def get_module_points(cls, module_id: int):
+        return cls.query.filter_by(module_id=module_id).all()
 
     def execute(self) -> int:
         if self.type & 1:  # HyperBlueprint
@@ -46,99 +45,22 @@ class Point(db.Model):
             pass
 
 
+class ModuleTypes(Enum):
+    STANDARD = 0
+    PRACTICE_BLOCK = 1
+    THEORY_BLOCK = 2
+    TEST = 3
+
+
 class Module(db.Model, Identifiable):
     __tablename__ = "modules"
+    not_found_text = "Module not found"
 
-    course_id = db.Column(db.Integer, primary_key=True)
-    module_id = db.Column(db.Integer, primary_key=True)
-
+    # Essentials
+    id = db.Column(db.Integer, primary_key=True)
+    length = db.Column(db.Integer, nullable=False)  # the amount of schedule or map points
     type = db.Column(db.Integer, nullable=False)  # 0 - standard; 1 - practice; 2 - theory; 3 - test
     name = db.Column(db.String(100), nullable=False)  # the name for the diagram (course map)
-    length = db.Column(db.Integer, nullable=False)  # the amount of schedule or map points
-
-    threshold = db.Column(db.Integer, nullable=False)  # points needed for starting
-    parents = db.Column(db.PickleType, nullable=False)  # Set[int] (module ids in the same course)
-    points = db.Column(db.Integer, nullable=False)  # points granted upon completion
-
-    @classmethod
-    def __create(cls, course_id: int, module_id: int, module_type: int, name: str,
-                 length: int, threshold: int, parents: Set[int], points: int):
-        if cls.find_by_ids(course_id, module_id):
-            return False
-        new_module = cls(course_id=course_id, module_id=module_id, type=module_type, name=name,
-                         length=length, threshold=threshold, parents=dumps(parents), points=points)
-        db.session.add(new_module)
-        db.session.commit()
-        return True
-
-    @classmethod
-    def find_by_ids(cls, course_id: int, module_id: int):
-        return cls.query.filter_by(course_id=course_id, module_id=module_id).first()
-
-    def is_access_granted(self, user_points: int, completed_modules: Set[int]) -> bool:
-        if user_points < self.threshold:
-            return False
-        return loads(self.parents).issubset(completed_modules)
-
-
-class Course(db.Model, Identifiable):  # same name courses are allowed  # development suspended
-    @staticmethod
-    def test():
-        Course.__create(0, "Математика ЕГЭ", "", 4, "math", "une", "enthusiast",
-                        2000, datetime(2020, 10, 22, 10, 30, 3))
-        Course.__create(1, "История ЕГЭ", "", 4, "history", "une", "enthusiast",
-                        1100, datetime(2021, 1, 2, 22, 30, 33))
-        Course.__create(2, "Арифметика", "", 4, "math", "middle-school", "newbie",
-                        100, datetime(2012, 10, 12, 15, 57, 2))
-        Course.__create(3, "Матан", "", 4, "math", "university", "amateur",
-                        0, datetime(1999, 3, 14, 6, 10, 5))
-        Course.__create(4, "English ABCs", "", 4, "languages", "hobby", "review",
-                        2000, datetime(2019, 7, 22, 22, 10, 45))
-        Course.__create(5, "Веб Дизайн", "", 4, "informatics", "prof-skills", "enthusiast",
-                        2000, datetime(2020, 10, 22, 10, 30, 8))
-        Course.__create(6, "Робототехника", "", 4, "informatics", "clubs", "newbie",
-                        3100, datetime(2021, 1, 2, 22, 30, 33))
-        Course.__create(7, "Архитектура XIX века", "", 4, "arts", "university", "expert",
-                        5, datetime(2012, 6, 12, 15, 57, 0))
-        Course.__create(8, "Безопасность в интернете", "", 4, "informatics", "university", "review",
-                        2002, datetime(1999, 3, 14, 6, 10, 5))
-        Course.__create(9, "Литература", "", 4, "literature", "bne", "enthusiast",
-                        300, datetime(2019, 7, 12, 22, 10, 40))
-        Course.__create(10, "Классическая Музыка", "", 4, "arts", "hobby", "enthusiast",
-                        2000, datetime(2019, 3, 22, 22, 10, 40))
-        Course.__create(11, "Немецкий язык", "", 4, "languages", "main-school", "enthusiast",
-                        700, datetime(2015, 7, 22, 22, 10, 40))
-        Course.__create(12, "География", "", 4, "geography", "hobby", "review",
-                        2000, datetime(2019, 7, 22, 22, 1, 40))
-        Course.__create(13, "Геодезия", "", 4, "geography", "hobby", "review",
-                        2000, datetime(2016, 7, 22, 2, 52, 40))
-        Course.__create(14, "Океанология", "", 4, "geography", "hobby", "review",
-                        2000, datetime(2019, 7, 22, 22, 46, 40))
-        Course.__create(15, "Ораторское искусство", "", 4, "arts", "prof-skills", "amateur",
-                        1200, datetime(2009, 7, 22, 22, 31, 0))
-        Course.__create(16, "Социология", "", 4, "social-science", "university", "review",
-                        2000, datetime(2012, 6, 12, 15, 57, 0))
-        Course.__create(17, "Классическая философия", "", 4, "philosophy", "hobby", "review",
-                        700, datetime(2019, 7, 22, 22, 11, 40))
-        Course.__create(18, "Физика: термодинамика", "", 4, "physics", "main-school", "review",
-                        4200, datetime(2012, 7, 22, 2, 10, 54))
-        Course.__create(19, "История России", "", 4, "history", "hobby", "review",
-                        270, datetime(2019, 7, 22, 22, 10, 24))
-        Course.__create(20, "Информатика 7 класс", "", 4, "informatics", "middle-school", "amateur",
-                        2000, datetime(2019, 7, 22, 22, 10, 12))
-        Course.__create(21, "Литература Европы XX века", "", 4, "literature", "hobby", "review",
-                        2000, datetime(2019, 5, 13, 1, 1, 54))
-        Course.__create(22, "Python", "", 4, "informatics", "clubs", "newbie",
-                        1500, datetime(2019, 7, 22, 22, 10, 32))
-
-    __tablename__ = "courses"
-    not_found_text = "Course not found"
-
-    # Vital stuff:
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    length = db.Column(db.Integer, nullable=False)
 
     # Filtering:
     theme = db.Column(db.String(20), nullable=False)
@@ -150,44 +72,36 @@ class Course(db.Model, Identifiable):  # same name courses are allowed  # develo
     creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     # Author-related
-    author_team = db.Column(db.Integer, db.ForeignKey("author-teams.id"), nullable=False,
-                            default=0)  # test-only
-
-    # File-related:
-    picture_type = db.Column(db.String(5), nullable=True, default="")
-    module_map = db.Column(db.String(100), nullable=True, default="")  # test
+    # ???
 
     @classmethod
-    def __create(cls, course_id: int, name: str, description: str, length: int, theme: str,
-                 category: str, difficulty: str, popularity: int, creation_date: datetime) -> bool:
-        if cls.find_by_id(course_id):
-            return False
-        new_course = cls(id=course_id, name=name, description=description, length=length,
-                         theme=theme, category=category, difficulty=difficulty,
-                         popularity=popularity, creation_date=creation_date)
-        db.session.add(new_course)
+    def __create(cls, module_type: ModuleTypes, name: str, length: int):
+        new_module = cls(type=module_type.value, name=name, length=length)
+        db.session.add(new_module)
         db.session.commit()
         return True
 
     @classmethod
-    def find_by_id(cls, entry_id: int):
-        return cls.query.filter_by(id=entry_id).first()
+    def find_by_id(cls, module_id: int):
+        return cls.query.filter_by(id=module_id).first()
 
     @classmethod
-    def get_course_list(cls, filters: Dict[str, str], user_id: int, offset: int, limit: int) -> list:
+    def get_module_list(cls, filters: Dict[str, str], user_id: int, offset: int, limit: int) -> list:
         query: BaseQuery = cls.query
-        query = query.filter(Course.id.notin_(CourseFilterSession.filter_ids_by_user(user_id, hidden=True)))
+        # explore joining queries!
+        query = query.filter(cls.id.notin_(ModuleFilterSession.filter_ids_by_user(user_id, hidden=True)))
 
         if filters is not None:
             keys: List[str] = list(filters.keys())
             if "global" in keys:
                 global_filter: str = filters["global"]
                 if global_filter == "pinned":
-                    query = query.filter(Course.id.in_(CourseFilterSession.filter_ids_by_user(user_id, pinned=True)))
+                    # joining queries!
+                    query = query.filter(cls.id.in_(ModuleFilterSession.filter_ids_by_user(user_id, pinned=True)))
                 elif global_filter == "starred":
-                    query = query.filter(Course.id.in_(CourseFilterSession.filter_ids_by_user(user_id, starred=True)))
+                    query = query.filter(cls.id.in_(ModuleFilterSession.filter_ids_by_user(user_id, starred=True)))
                 elif global_filter == "started":
-                    query = query.filter(Course.id.in_(CourseFilterSession.filter_ids_by_user(user_id, started=True)))
+                    query = query.filter(cls.id.in_(ModuleFilterSession.filter_ids_by_user(user_id, started=True)))
 
             if "difficulty" in keys:
                 query = query.filter_by(difficulty=filters["difficulty"])
@@ -198,47 +112,12 @@ class Course(db.Model, Identifiable):  # same name courses are allowed  # develo
 
         return query.offset(offset).limit(limit).all()
 
-    @classmethod
-    def get_advanced_course_list(cls, filters: Dict[str, List[str]], search: str,
-                                 user_id: int, offset: int, limit: int) -> list:
-        query: BaseQuery = cls.query
-        query = query.filter(CourseFilterSession.filter_ids_by_user(user_id, hidden=True))
-
-        if filters is not None:
-            if "global" in filters.keys():
-                global_filters = filters["global"]
-                if "pinned" in global_filters:
-                    query = query.filter(CourseFilterSession.filter_ids_by_user(user_id, pinned=True))
-                if "starred" in global_filters:
-                    query = query.filter(CourseFilterSession.filter_ids_by_user(user_id, starred=True))
-                if "started" in global_filters:
-                    query = query.filter(CourseFilterSession.filter_ids_by_user(user_id, started=True))
-
-            if "difficulty" in filters.keys() and len(filters["difficulty"]):
-                query = query.filter(Course.difficulty.in_(filters["difficulty"]))
-            if "category" in filters.keys() and len(filters["category"]):
-                query = query.filter(Course.category.in_(filters["category"]))
-            if "theme" in filters.keys() and len(filters["theme"]):
-                query = query.filter(Course.theme.in_(filters["theme"]))
-
-        if search is not None and len(search):
-            query = query.filter(Course.name.like(f"%{search}%"))
-
-        return query.offset(offset).limit(limit).all()
-
-    def update_available_modules(self, user_points: int, completed_modules: Set[int],
-                                 available_modules: Set[int]):
-        for i in set(range(self.length)).difference(available_modules):
-            module: Module = Module.find_by_ids(self.id, i)
-            if module.is_access_granted(user_points, completed_modules):
-                available_modules.add(i)
-
     def to_short_json(self) -> dict:
-        return {"id": self.id, "name": self.name, "author": self.author_team}
+        pass
 
     def to_json(self, user_id: int = None) -> dict:
         result: dict = self.to_short_json()
         if user_id is not None:
-            result.update(CourseFilterSession.find_json(user_id, self.id))
-        result.update({"category": self.category, "theme": self.theme, "difficulty": self.difficulty})
+            result.update()
+        result.update()
         return result
