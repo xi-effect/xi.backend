2666679463ff94ad346ece7caa4803ae6ff99f98
qwert45hi <qwert45hi@yandex.ru>
2021-07-10 23:18:49 +0300

    New auto-update system

--- api_resources/outside/updater.py
+++ api_resources/outside/updater.py
@@ -1,5 +1,5 @@
 from api_resources.base.checkers import argument_parser
-from webhooks import send_discord_message, WebhookURLs, execute_in_console, disable_webapp
+from webhooks import send_discord_message, WebhookURLs, execute_in_console, reload_webapp
 
 from flask_restful import Resource
 from flask_restful.reqparse import RequestParser
@@ -16,8 +16,8 @@ class GithubWebhook(Resource):  # /update/
         if event_type == "push":
             send_discord_message(WebhookURLs.GITHUB, f"Got a push notification.\n"
                                                      f"Starting auto-update")
-            execute_in_console("python updater.py")
-            disable_webapp()
+            execute_in_console("git pull")
+            reload_webapp()
         elif event_type == "release":
             send_discord_message(WebhookURLs.GITHUB, f"Got a release notification.\n"
                                                      f"Releases are not supported yet!")
