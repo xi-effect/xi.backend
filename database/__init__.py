73b089e79df1073bb356e48a68bee22b7fe24f36
qwert45hi <qwert45hi@yandex.ru>
2021-07-10 20:33:41 +0300

    database.__init__.py fix

--- database/__init__.py
+++ database/__init__.py
@@ -4,10 +4,10 @@ from database.education.courses import Module, ModuleType, Point
 from database.education.sessions import ModuleFilterSession, StandardModuleSession, TestModuleSession
 from database.education.moderation import CATSubmission
 
-from database.file_system.keeper import CATFile, CATCourse, Page
+from database.file_system.keeper import CATFile, Page
 
 from database.outside.tester import TestPoint, UserSubmissions, ResultCodes  # other
 
 from database.users.users import User, TokenBlockList
-from database.users.authors import Author, AuthorTeam
+from database.users.authors import Author
 from database.users.special import Moderator
