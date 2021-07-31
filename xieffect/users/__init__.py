a2825c67137b66a746506b24c48a67a467e0949a
qzart45hi <qwert45hi@yandex.ru>
2021-07-31 03:27:20 +0300

    feat: adding GET /settings/roles/ and POST /authors/ to api

--- xieffect/users/__init__.py
+++ xieffect/users/__init__.py
@@ -1,4 +1,4 @@
 from .database import User, TokenBlockList
 from .confirmer import EmailSender, EmailConfirm
 from .reglog import UserRegistration, UserLogin, UserLogout, PasswordResetSender, PasswordReseter
-from .settings import Avatar, Settings, MainSettings, EmailChanger, PasswordChanger
+from .settings import Avatar, Settings, MainSettings, RoleSettings, EmailChanger, PasswordChanger
