3284d8253a0cbfbed431bd421f5406b411a58a04
qzart45hi <qwert45hi@yandex.ru>
2021-08-25 02:02:26 +0300

    feat: making wip tests usable with any WIP JSON file

--- xieffect/test/authorship_test.py
+++ xieffect/test/authorship_test.py
@@ -7,36 +7,40 @@ from pytest import mark
 from xieffect.test.components import check_status_code
 
 
+def check_creating(client: FlaskClient, list_tester: Callable[[str, dict, int], Iterator[list]], wip_type: str):
+    with open(f"xieffect/test/json/sample-{wip_type}.json", "rb") as f:
+        content: dict = load(f)
+
+    assert (content_id := check_status_code(client.post(f"/wip/{wip_type}s", json=content)).get("a", None))
+    assert any(content_id == data["id"] for page in list_tester(f"/wip/{wip_type}s/index", {}, 20) for data in page)
+
+    content["id"] = content_id
+    assert check_status_code(client.get(f"/wip/{wip_type}s/{content_id}", json=content)) == content
+
+
+def check_editing(client: FlaskClient, wip_type: str):
+    with open(f"xieffect/test/json/sample-{wip_type}.json", "rb") as f:
+        content: dict = load(f)
+    assert (content_id := check_status_code(client.post(f"/wip/{wip_type}s", json=content)).get("a", None))
+
+    page: dict = check_status_code(client.get(f"/wip/{wip_type}s/{content_id}"))
+    assert content_id == page["id"]
+
+    with open(f"xieffect/test/json/sample-{wip_type}-2.json", "rb") as f:
+        edited_content: dict = load(f)
+    assert edited_content != content
+    edited_content["id"] = content_id
+    assert page != edited_content
+
+    check_status_code(client.put(f"/wip/{wip_type}s/{content_id}", json=edited_content), get_json=False)
+    assert check_status_code(client.get(f"/wip/{wip_type}s/{content_id}", json=edited_content)) == edited_content
+
+
 @mark.order(10)
 def test_wip_page_creating(client: FlaskClient, list_tester: Callable[[str, dict, int], Iterator[list]]):
-    with open("xieffect/test/json/sample-page.json", "rb") as f:
-        sample_page: dict = load(f)
-
-    assert (page_id := check_status_code(client.post("/wip/pages", json=sample_page)).get_json().get("a", None))
-    assert any(page_id == data["id"] for page_page in list_tester("/wip/pages/index", {}, 20) for data in page_page)
-
-    sample_page["id"] = page_id
-    assert check_status_code(client.get(f"/wip/pages/{page_id}", json=sample_page)).get_json() == sample_page
+    check_creating(client, list_tester, "page")
 
 
 @mark.order(11)
-def test_wip_page_editing(client: FlaskClient, list_tester: Callable[[str, dict, int], Iterator[list]]):
-    with open("xieffect/test/json/sample-page.json", "rb") as f:
-        sample_page: dict = load(f)
-    assert (page_id := check_status_code(client.post("/wip/pages", json=sample_page)).get_json().get("a", None))
-
-    page: dict = check_status_code(client.get(f"/wip/pages/{page_id}")).get_json()
-    assert page_id == page["id"]
-
-    with open("xieffect/test/json/sample-page-2.json", "rb") as f:
-        edited_page: dict = load(f)
-    assert edited_page != sample_page
-    edited_page["id"] = page_id
-    assert page != edited_page
-
-    check_status_code(client.put(f"/wip/pages/{page_id}", json=edited_page))
-    assert check_status_code(client.get(f"/wip/pages/{page_id}", json=edited_page)).get_json() == edited_page
-
-
-def test_wip_module(client: FlaskClient):
-    pass
+def test_wip_page_editing(client: FlaskClient):
+    check_editing(client, "page")
