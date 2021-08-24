3878c71a85666e3c4b79114aa0332b72e128eddc
qzart45hi <qwert45hi@yandex.ru>
2021-08-25 00:55:30 +0300

    feat: marking test order

--- xieffect/test/error_test.py
+++ xieffect/test/error_test.py
@@ -1,12 +1,15 @@
+from pytest import mark
 from flask import Response
 from flask.testing import FlaskClient
 
 
+@mark.order(8)
 def test_missing_url(client: FlaskClient):
     response: Response = client.get("/this-does-not-exist")
     assert response.status_code == 404
 
 
+@mark.order(9)
 def test_missing_module(client: FlaskClient):
     response: Response = client.post("/modules/-1/report/", json={"reason": "It's so negative!"})
     assert response.status_code == 404
--- xieffect/test/settings_test.py
+++ xieffect/test/settings_test.py
@@ -1,7 +1,9 @@
+from pytest import mark
 from flask.testing import FlaskClient
 from flask.wrappers import Response
 
 
+@mark.order(3)
 def test_getting_settings(client: FlaskClient):
     response: Response = client.get("/settings", follow_redirects=True)
     assert response.status_code == 200
@@ -11,5 +13,6 @@ def test_getting_settings(client: FlaskClient):
                            "patronymic", "dark-theme", "language"))
 
 
+@mark.order(4)
 def test_changing_settings(client: FlaskClient):
     pass
