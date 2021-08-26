465b343f04fdc9d0eb8a6d94d957220a1ea2b5ad
qzart45hi <qwert45hi@yandex.ru>
2021-08-26 19:32:10 +0300

    feat: adding PageKinds and storing them

--- xieffect/education/elements.py
+++ xieffect/education/elements.py
@@ -12,6 +12,16 @@ from education.sessions import ModuleFilterSession
 from main import db, whooshee
 
 
+class PageKind(Enum):
+    THEORY = 0
+    PRACTICE = 1
+    TASK = 2
+
+    @classmethod
+    def from_string(cls, string):
+        return cls.__members__[string.upper()]
+
+
 @whooshee.register_model("name", "theme", "description")
 class Page(db.Model, Identifiable):
     @staticmethod
@@ -40,6 +50,7 @@ class Page(db.Model, Identifiable):
 
     @classmethod
     def _create(cls, json_data: Dict[str, Union[str, int, bool, list]]):
+        json_data["kind"] = PageKind.from_string(json_data["kind"]).value
         entry: cls = cls(**{key: json_data[key] for key in ("id", "kind", "name", "theme", "description",
                                                             "reusable", "public", "blueprint")})
         db.session.add(entry)
@@ -75,8 +86,9 @@ class Page(db.Model, Identifiable):
         return query.offset(start).limit(limit).all()  # redo with pagination!!!
 
     def to_json(self):
-        return {"id": self.id, "name": self.name, "description": self.description, "theme": self.theme,
-                "kind": "practice", "blueprint": self.blueprint, "reusable": self.reusable, "public": self.public}
+        return {"id": self.id, "name": self.name, "description": self.description,
+                "theme": self.theme, "kind": PageKind(self.kind).name.lower(),
+                "blueprint": self.blueprint, "reusable": self.reusable, "public": self.public}
 
 
 class Point(db.Model):
