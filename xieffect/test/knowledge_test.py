d0281fe74b96015933799f8a7a5ea615c940083b
qzart45hi <qwert45hi@yandex.ru>
2021-08-26 11:46:30 +0300

    feat: add /pages/<ID>/ & /pages/<ID>/components/ test

--- xieffect/test/knowledge_test.py
+++ xieffect/test/knowledge_test.py
@@ -20,3 +20,12 @@ def test_pinned_modules(client: FlaskClient, list_tester: Callable[[str, dict, i
     for response_json in list_tester("/modules", {"filters": {"global": "pinned"}}, 12):
         module_ids.extend([module["id"] for module in response_json])
     assert 3 in module_ids
+
+
+@mark.order(406)
+def test_module_list(client: FlaskClient):
+    page_json: dict = check_status_code(client.get("/pages/1"))
+    page_json["components"] = check_status_code(client.get("/pages/1/components"))
+
+    with open("files/tfs/test/1.json", "rb") as f:
+        assert page_json == load(f)
