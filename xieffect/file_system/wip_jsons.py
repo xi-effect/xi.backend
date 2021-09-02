a3eec1c6e6d0c2af338ef7ac3c666abdddf81fd9
qzart45hi <qwert45hi@yandex.ru>
2021-09-02 23:48:55 +0300

    feat: redoing /wip/<type>/ resources for json-files only

---
+++ xieffect/file_system/wip_jsons.py
@@ -0,0 +1,88 @@
+from typing import Type
+from json import load
+
+from flask import request  # , send_from_directory
+from flask_restful import Resource
+
+from authorship import Author
+from componets import jwt_authorizer, lister, database_searcher
+from education import Page
+from .keeper import JSONFile, WIPModule, WIPPage
+
+
+def file_getter(type_only: bool = True):
+    def file_getter_wrapper(function):
+        @jwt_authorizer(Author, "author")
+        def get_file_or_type(*args, **kwargs):
+            result: Type[JSONFile]
+            file_type: str = kwargs.pop("file_type")
+            if file_type == "modules":
+                result = WIPModule
+            elif file_type == "pages":
+                result = WIPPage
+            else:
+                return {"a": f"File type '{file_type}' is not supported"}, 400
+
+            if "file_id" in kwargs.keys():
+                file: result = result.find_by_id(kwargs["file_id"] if type_only else kwargs.pop("file_id"))
+                if file.owner != kwargs.pop("author").id:
+                    return {"a": "Access denied"}, 403
+                if not type_only:
+                    return function(file=file, *args, **kwargs)
+            return function(file_type=result, *args, **kwargs)
+
+        return get_file_or_type
+
+    return file_getter_wrapper
+
+
+class FileLister(Resource):  # [POST] /wip/<file_type>/index/
+    @file_getter()
+    @lister(20)
+    def post(self, file_type: Type[JSONFile], author: Author, start: int, finish: int):
+        if WIPPage not in file_type.mro():
+            return {"a": f"File type '{file_type}' is not supported"}, 400
+        return [x.get_metadata() for x in file_type.find_by_owner(author, start, finish - start)]
+
+
+class FileCreator(Resource):  # [POST] /wip/<file_type>/
+    @file_getter()
+    def post(self, author: Author, file_type: Type[JSONFile]):
+        result: file_type = file_type.create_from_json(author, request.get_json())
+        # for CATFile  result: file_type = file_type.create_with_file(author, request.get_data())
+        return {"id": result.id}
+
+
+class FileProcessor(Resource):  # [GET|PUT|DELETE] /wip/<file_type>/<int:file_id>/
+    @file_getter(type_only=False)
+    def get(self, file: JSONFile):
+        with open(file.get_link(), "rb") as f:
+            result = load(f)
+        return result
+
+    # @file_getter()  # PermissionError(13)
+    # def get(self, file_type: Type[CATFile], file_id: int):
+    #     return send_from_directory("../" + file_type.directory, f"{file_id}.{file_type.mimetype}")
+
+    @file_getter(type_only=False)
+    def put(self, file: JSONFile):
+        file.update_json(request.get_json())
+        # file.update(request.get_data())
+        return {"a": True}
+
+    @file_getter(type_only=False)
+    def delete(self, file: JSONFile):
+        file.delete()
+        return {"a": True}
+
+
+class PagePublisher(Resource):  # POST /wip/pages/<int:page_id>/publication/
+    @jwt_authorizer(Author, "author")
+    @database_searcher(WIPPage, "page_id", "wip_page")
+    def post(self, author: Author, wip_page: WIPPage):
+        if wip_page.owner != author.id:
+            return {"a": "Access denied"}, 403
+
+        with open(wip_page.get_link()) as f:
+            result: bool = Page.create(load(f), author) is None
+        return {"a": "Page already exists" if result else "Success"}
