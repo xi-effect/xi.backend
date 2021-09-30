25647f3fc1128eca29a8edc38e06998845501d59
qzart45hi <qwert45hi@yandex.ru>
2021-10-01 01:56:54 +0300

    feat(testing): starting with WIPRecycler

---
+++ xieffect/test/wip_process_test.py
@@ -0,0 +1,60 @@
+from json import load
+from typing import Callable, Iterator, Union, Optional
+
+from flask.testing import FlaskClient
+from pytest import mark
+
+from xieffect.test.components import check_status_code
+
+PER_REQUEST = 50
+
+
+# https://discord.com/channels/706806130348785715/843536940083314728/880041704651108432
+
+
+class WIPRecycler:
+    def __init__(self, client: FlaskClient, file_type: str, file_path: str,
+                 list_tester: Callable[[str, dict, int], Iterator[dict]]):
+        self.client: FlaskClient = client
+        self.file_type: str = file_type
+        self.wip_url: str = f"/wip/{self.file_type}"
+        self.file_id: Optional[Union[int, str]] = None
+
+        def list_iterator():
+            return list_tester(self.wip_url + "/index/", {}, PER_REQUEST)
+
+        self.list_iterator: Callable[[], Iterator[dict]] = list_iterator
+
+        with open(file_path, "rb") as f:
+            self.file_content = load(f)
+
+    def is_in_list(self) -> bool:
+        return any(file["id"] == self.file_id for file in self.list_iterator())
+
+    def creating(self):
+        pass
+
+    def editing(self):
+        pass
+
+    def publishing(self):
+        pass
+
+    def deleting(self):
+        pass
+
+    def wip_full_cycle(self):
+        self.creating()
+        self.editing()
+        self.publishing()
+        self.deleting()
+
+
+@mark(200)
+def test_pages(client: FlaskClient, list_tester: Callable[[str, dict, int], Iterator[dict]]):
+    WIPRecycler(client, "pages", "lululululul", list_tester).wip_full_cycle()
+
+
+@mark(220)
+def test_modules(client: FlaskClient, list_tester: Callable[[str, dict, int], Iterator[dict]]):
+    WIPRecycler(client, "modules", "lululululul", list_tester).wip_full_cycle()
