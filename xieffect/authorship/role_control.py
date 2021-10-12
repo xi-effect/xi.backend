25ec7781650fd97c5171caaaf723aa926ead4aca
sipmine <gleb.tedpoope@gmail.com>
2021-10-12 22:57:01 +0700

    feat: mod command (ban & unban)

--- xieffect/authorship/role_control.py
+++ xieffect/authorship/role_control.py
@@ -2,7 +2,7 @@ from flask_restx import Resource
 
 from componets import Namespace
 from users import User
-from .user_roles import Author
+from .user_roles import Author, Moderator
 
 authors_namespace: Namespace = Namespace("authors", path="/authors")
 
@@ -15,3 +15,21 @@ class AuthorInitializer(Resource):  # [GET] /authors/permit/
         """ Adds Author role to the User (requester).
         Does nothing, if it has been added before. Will fail if Author was banned. """
         return Author.initialize(session, user)
+
+
+@authors_namespace.route("/<int:author.id>/ban/")
+class BanAuthor(Resource):
+    @authors_namespace.a_response()
+    @authors_namespace.jwt_authorizer(Moderator, chek_only=True)
+    def post(self, session, author_id: int):
+        author: Author = Author.find_by_id(session, author_id)
+        author.banned = True
+
+
+@authors_namespace.route("/<int:author.id>/unban/")
+class BanAuthor(Resource):
+    @authors_namespace.a_response()
+    @authors_namespace.jwt_authorizer(Moderator, chek_only=True)
+    def post(self, session, author_id: int):
+        author: Author = Author.find_by_id(session, author_id)
+        author.banned = False
