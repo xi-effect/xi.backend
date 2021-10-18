36d5983e500834c01b9de31c4f21eb712b1e3bd9
qzart45hi <qwert45hi@yandex.ru>
2021-10-18 16:00:14 +0300

    feat: deployment files for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: sh -c 'cd xieffect && gunicorn wsgi:app'
--- xieffect/webhooks/__init__.py
+++ xieffect/webhooks/__init__.py
@@ -1,4 +1,4 @@
-from .anywherer import execute_in_console, execute_script_in_console
-from .anywherer import webapp_action, enable_webapp, disable_webapp, reload_webapp
+# from .anywherer import execute_in_console, execute_script_in_console
+# from .anywherer import webapp_action, enable_webapp, disable_webapp, reload_webapp
 from .discorder import send_file_message as send_file_discord_message
 from .discorder import send_message as send_discord_message, WebhookURLs
