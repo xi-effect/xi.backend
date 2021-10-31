5910593cf2eb080d07b8ead3ab9b9850aab61b92
qzart45hi <qwert45hi@yandex.ru>
2021-10-31 18:23:57 +0300

    feat(testing): test for user search

--- xieffect/test/profiles_test.py
+++ xieffect/test/profiles_test.py
@@ -1,9 +1,22 @@
+from typing import Iterator, Callable
+
 from flask.testing import FlaskClient
 
 from .components import check_status_code
 
 
-def test_profiles_user(client: FlaskClient, ):
+def test_user_search(client: FlaskClient, list_tester: Callable[[str, dict, int], Iterator[dict]]):
+    results = list(list_tester("/users/", {}, 10))
+
+    for res in results:
+        assert res["username"] != "test"
+        if res["username"] == "admin":
+            break
+    else:
+        assert False, "Admin user not found"
+
+
+def test_profiles_user(client: FlaskClient):
     check_status_code(client.post("/settings/", json={"changed": {"name": "Danila", "surname": "Petrov",
                                                                   "patronymic": "Danilovich", "bio": "Pricol",
                                                                   "group": "3B"}}))
