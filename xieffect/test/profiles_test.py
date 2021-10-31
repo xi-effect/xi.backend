04c27e0e3b5b1c7211661b180e30ee5f3506ade2
qzart45hi <qwert45hi@yandex.ru>
2021-10-31 18:29:53 +0300

    fix(testing): dryifying test_user_profile

--- xieffect/test/profiles_test.py
+++ xieffect/test/profiles_test.py
@@ -16,24 +16,18 @@ def test_user_search(client: FlaskClient, list_tester: Callable[[str, dict, int]
         assert False, "Admin user not found"
 
 
-def test_profiles_user(client: FlaskClient):
-    check_status_code(client.post("/settings/", json={"changed": {"name": "Danila", "surname": "Petrov",
-                                                                  "patronymic": "Danilovich", "bio": "Pricol",
-                                                                  "group": "3B"}}))
+def test_user_profile(client: FlaskClient):
+    new_settings: dict[str, str] = {
+        "name": "Danila",
+        "surname": "Petrov",
+        "patronymic": "Danilovich",
+        "bio": "Pricol",
+        "group": "3B"
+    }
 
+    check_status_code(client.post("/settings/", json={"changed": new_settings}))
     data: dict = check_status_code(client.get("/users/1/profile"))
 
-    assert "name" in data.keys()
-    assert data["name"] == "Danila"
-
-    assert "surname" in data.keys()
-    assert data["surname"] == "Petrov"
-
-    assert "patronymic" in data.keys()
-    assert data["patronymic"] == "Danilovich"
-
-    assert "bio" in data.keys()
-    assert data["bio"] == "Pricol"
-
-    assert "group" in data.keys()
-    assert data["group"] == "3B"
+    for key, value in new_settings.items():
+        assert key in data.keys()
+        assert data[key] == value
