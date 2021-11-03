60530cc3b2ff0481e2e7340d76fcfa4634cc0152
qzart45hi <qwert45hi@yandex.ru>
2021-11-03 14:57:38 +0300

    feat(testing): new settings_test

--- xieffect/test/settings_test.py
+++ xieffect/test/settings_test.py
@@ -6,11 +6,31 @@ from xieffect.test.components import check_status_code
 
 @mark.order(100)
 def test_getting_settings(client: FlaskClient):
-    data: dict = check_status_code(client.get("/settings", follow_redirects=True))
+    data: dict = check_status_code(client.get("/settings/"))
     for key in ("email", "email-confirmed", "username", "dark-theme", "language"):
         assert key in data.keys()
 
 
 @mark.order(101)
 def test_changing_settings(client: FlaskClient):
-    pass
+    new_settings = {
+        "username": "hey",
+        "dark-theme": False,
+        "avatar": {
+            "cool": "avatar",
+            "hello": "world",
+        },
+    }
+
+    old_settings = check_status_code(client.get("/settings/"))
+    assert all(old_settings.get(key, None) != setting for key, setting in new_settings.items())
+
+    check_status_code(client.post("/settings/", json={"changed": new_settings}))
+
+    result_settings = check_status_code(client.get("/settings/"))
+    assert all(result_settings[key] == setting for key, setting in new_settings.items())
+
+    check_status_code(client.post("/settings/", json={"changed": {
+        key: old_settings.get(key, None) for key in new_settings.keys()}}))
+    result_settings = check_status_code(client.get("/settings/"))
+    assert all(result_settings[key] == setting for key, setting in old_settings.items())
