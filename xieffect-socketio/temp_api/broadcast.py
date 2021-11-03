8f13c9cf2f308e2c91e593f47fdb283a386e2218
qzart45hi <qwert45hi@yandex.ru>
2021-11-03 18:40:30 +0300

    feat: chat-specific rooms

--- xieffect-socketio/temp_api/broadcast.py
+++ xieffect-socketio/temp_api/broadcast.py
@@ -16,7 +16,7 @@ def broadcast(event: str, data: dict, *user_ids: int):
 
 
 def room_broadcast(event: str, data: dict, room: str, namespace: str = "/"):
-    socketio.emit(event, data, to=room, broadcast=True, namespace=namespace)
+    socketio.emit(event, data, to=room, namespace=namespace)
 
 
 @broadcast_namespace.route("/<event>/")
--- xieffect-socketio/websockets/messaging.py
+++ xieffect-socketio/websockets/messaging.py
@@ -1,15 +1,30 @@
-from flask_jwt_extended import jwt_required
-from flask_socketio import Namespace
+from requests import post, put, delete
+
+from flask import request
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_socketio import join_room, leave_room, Namespace
+
+from setup import socketio, storage, app
 
 
 class MessagesNamespace(Namespace):
     @jwt_required()  # if not self.authenticate(request.args): raise ConnectionRefusedError("unauthorized!")
     def on_connect(self, _):
-        pass
+        storage[get_jwt_identity()] = request.sid
 
     @jwt_required()
     def on_disconnect(self):
-        pass
+        storage.pop(get_jwt_identity())
+
+    @jwt_required()
+    def on_open(self, data):
+        chat_id: int = data["chat-id"]
+        join_room(f"chat-{chat_id}")
+
+    @jwt_required()
+    def on_close(self, data):
+        chat_id: int = data["chat-id"]
+        leave_room(f"chat-{chat_id}")
 
     def on_send(self, data):
         pass
@@ -18,10 +33,4 @@ class MessagesNamespace(Namespace):
         pass
 
     def on_delete(self, data):
-        pass
-
-    def on_open(self, data):
-        pass
-
-    def on_close(self, data):
         pass
