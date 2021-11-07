4f400cf80e5074798bcbbc1eb86e37ff1e772312
qzart45hi <qwert45hi@yandex.ru>
2021-11-07 21:44:43 +0300

    feat: add with_arguments decorator for events

--- xieffect-socketio/websockets/library.py
+++ xieffect-socketio/websockets/library.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 from functools import wraps
+from typing import Optional
 
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_socketio import Namespace as _Namespace
@@ -9,6 +11,53 @@ from setup import socketio, user_sessions
 
 def room_broadcast(event: str, data: dict, room: str, namespace: str = "/"):
     socketio.emit(event, data, to=room, namespace=namespace)
+
+
+@dataclass()
+class EventArgument:
+    name: str
+    dest: Optional[str] = None
+    desc: Optional[str] = None
+
+    def __post_init__(self):
+        if self.dest is None:
+            self.dest = self.name.replace("-", "_")
+
+
+def with_arguments(*event_args: EventArgument):
+    def with_arguments_wrapper(function):
+        @wraps(function)
+        def with_arguments_inner(*args, **kwargs):
+            try:
+                data: dict = kwargs.pop("data")
+                kwargs.update({arg.dest: data[arg.name] for arg in event_args})
+                return function(*args, **kwargs)
+            except KeyError as e:
+                pass  # error check
+
+        return with_arguments_inner
+
+    return with_arguments_wrapper
+
+
+@dataclass()
+class RequestException(Exception):
+    code: int
+    message: str = None
+    client_fault: bool = None
+    server_fault: bool = None
+
+    def __post_init__(self):
+        self.client_fault = self.code in (401, 403, 404, 422)
+        self.server_fault = self.code in (400, 401, 422)
+
+    def emit_error_event(self, event_name: str):
+        if self.server_fault:
+            send_discord_message("errors", f"Socket-IO server {self.code}-error appeared!\n`{self.message}`")
+        if self.client_fault:
+            emit_error(self.message, event_name, code=self.code)
+        else:
+            emit_error("Server error appeared. It was reported to discord", event_name, code=500)
 
 
 class Session(_Session):
