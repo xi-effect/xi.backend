cfa44e17b0d68840c3fae051e47feb4799a2d13a
qzart45hi <qwert45hi@yandex.ru>
2021-11-07 22:14:02 +0300

    feat: add full error check & automatic error-event

--- xieffect-socketio/websockets/library.py
+++ xieffect-socketio/websockets/library.py
@@ -1,16 +1,29 @@
 from dataclasses import dataclass
+from datetime import datetime
 from functools import wraps
+from json import dumps
 from typing import Optional
 
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from flask_socketio import Namespace as _Namespace
-from requests import Session as _Session, Response
+from flask_socketio import emit, Namespace as _Namespace
+from requests import Session as _Session, Response, post
 
-from setup import socketio, user_sessions
+from setup import socketio, user_sessions, app
 
 
 def room_broadcast(event: str, data: dict, room: str, namespace: str = "/"):
     socketio.emit(event, data, to=room, namespace=namespace)
+
+
+def send_discord_message(webhook: str, message: str):
+    host = "http://localhost:5000" if app.debug else "https://xieffect.pythonanywhere.com"
+    post(f"{host}/webhooks/pass-through/", headers={"Authorization": app.config["API_KEY"]},
+         json={"webhook": webhook, "message": message})
+
+
+def emit_error(message: str, event_name: str, **kwargs):
+    kwargs.update({"a": message, "event": event_name, "timestamp": datetime.utcnow().isoformat()})
+    emit("error", kwargs)
 
 
 @dataclass()
@@ -65,7 +78,10 @@ class Session(_Session):
     def request(self, *args, **kwargs):
         response: Response = super(Session, self).request(*args, **kwargs)
         if 400 <= response.status_code < 500:
-            pass  # error check
+            if (a := response.json().get("a", None)) is not None:
+                raise RequestException(response.status_code, a)
+            else:
+                raise RequestException(response.status_code)
         return response
 
 
@@ -74,7 +90,10 @@ def with_request_session(function):
     @wraps(function)
     def with_request_session_inner(*args, **kwargs):
         kwargs["session"] = user_sessions.sessions[get_jwt_identity()]
-        return function(*args, **kwargs)
+        try:
+            return function(*args, **kwargs)
+        except RequestException as e:
+            e.emit_error_event(function.__name__)
 
     return with_request_session_inner
 
@@ -82,3 +101,7 @@ def with_request_session(function):
 class Namespace(_Namespace):
     def trigger_event(self, event, *args):
         super().trigger_event(event.replace("-", "_"), *args)
+
+    def on_error(self, data):
+        send_discord_message("errors", f"Client reported socketio error!\n"
+                                       f"```json\n{dumps(data, ensure_ascii=False, indent=2)}\n```")
