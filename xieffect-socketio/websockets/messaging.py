7153cb52312e0494c9a05adff370d12e952d98ee
qzart45hi <qwert45hi@yandex.ru>
2021-11-07 22:07:16 +0300

    feat: utilize new decorators to replace old parse_chat_data

--- xieffect-socketio/websockets/messaging.py
+++ xieffect-socketio/websockets/messaging.py
@@ -1,35 +1,9 @@
-from functools import wraps
-
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_socketio import join_room, leave_room, rooms
 from jwt import decode
-from requests import Session
 
 from setup import user_sessions, app
-from .library import room_broadcast, Namespace
-
-
-def parse_chat_data(function):
-    @jwt_required()
-    @wraps(function)
-    def parse_chat_data_inner(self, data: dict):
-        chat_id: int = data.get("chat-id")
-        args: list = [chat_id]
-
-        url = f"{self.host}/chats/{chat_id}/"
-        if (message_id := data.get("message-id", None)) is not None:
-            url += f"messages/{message_id}/"
-        elif (user_id := data.get("user-id", None)) is not None:
-            url += f"users/{user_id}/"
-            args.append(user_id)
-            if (role := data.get("role", None)) is not None:
-                args.append(role)
-        else:
-            url += "messages/" if "content" in data.keys() else "presence/"
-
-        return function(self, url, *args, user_sessions.sessions[get_jwt_identity()], data)
-
-    return parse_chat_data_inner
+from .library import room_broadcast, Namespace, with_request_session, with_arguments, EventArgument as EArg, Session
 
 
 def get_identity(jwt_cookie: str) -> int:
@@ -54,27 +28,32 @@ class MessagesNamespace(Namespace):
         chat_ids = [int(chat_id) for room_name in rooms() if (chat_id := room_name.partition("chat-")[2]) != ""]
         session.post(f"{self.host}/chats/close-all/", json={"ids": chat_ids})
 
-    @parse_chat_data
-    def on_open(self, url: str, chat_id: int, session: Session, _):
-        session.post(url, json={"online": True})
+    @with_request_session
+    @with_arguments(EArg("chat-id"))
+    def on_open(self, session: Session, chat_id: int):
+        session.post(f"{self.host}/chats/{chat_id}/presence/", json={"online": True})
         join_room(f"chat-{chat_id}")
 
-    @parse_chat_data
-    def on_close(self, url: str, chat_id: int, session: Session, _):
-        session.post(url, json={"online": False})
+    @with_request_session
+    @with_arguments(EArg("chat-id"))
+    def on_close(self, session: Session, chat_id: int):
+        session.post(f"{self.host}/chats/{chat_id}/presence/", json={"online": False})
         leave_room(f"chat-{chat_id}")
 
-    @parse_chat_data
-    def on_send(self, url: str, chat_id: int, session: Session, data: dict):
-        session.post(url, json=data)  # add error check
+    @with_request_session
+    @with_arguments(EArg("chat-id"), EArg("content", check_only=True), use_original_data=True)
+    def on_send(self, session: Session, chat_id: int, data: dict):
+        session.post(f"{self.host}/chats/{chat_id}/messages/", json=data)
         room_broadcast("send", data, f"chat-{chat_id}")
 
-    @parse_chat_data
-    def on_edit(self, url: str, chat_id: int, session: Session, data: dict):
-        session.put(url, json=data)  # add error check
+    @with_request_session
+    @with_arguments(EArg("chat-id"), EArg("message-id"), EArg("content", check_only=True), use_original_data=True)
+    def on_edit(self, session: Session, chat_id: int, message_id: int, data: dict):
+        session.put(f"{self.host}/chats/{chat_id}/messages/{message_id}/", json=data)
         room_broadcast("edit", data, f"chat-{chat_id}")
 
-    @parse_chat_data
-    def on_delete(self, url: str, chat_id: int, session: Session, data: dict):
-        session.delete(url)  # add error check
-        room_broadcast("delete", data, f"chat-{chat_id}")
+    @with_request_session
+    @with_arguments(EArg("chat-id"), EArg("message-id"), use_original_data=True)
+    def on_delete(self, session: Session, chat_id: int, message_id: int, data: dict):
+        session.delete(f"{self.host}/chats/{chat_id}/messages/{message_id}/")
+        room_broadcast("delete", data, f"chat-{chat_id}")  # add "message" to data!!!
