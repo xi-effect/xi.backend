0a82ca825648848146b5d85e9fe52fc0e312fe19
qzart45hi <qwert45hi@yandex.ru>
2021-11-13 16:12:12 +0300

    feat: documenting events & collecting docs from events

--- xieffect-socketio/websockets/auto_doc.py
+++ xieffect-socketio/websockets/auto_doc.py
@@ -2,14 +2,27 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Type, Any, Callable
 
-from flask_socketio import emit, Namespace as _Namespace
+from flask_socketio import emit, Namespace as _Namespace, SocketIO as _SocketIO
 from pydantic import BaseModel
+
+
+def remove_none(data: dict):
+    return {key: value for key, value in data.items() if value is not None}
 
 
 @dataclass()
 class Event:
     model: Type[BaseModel]
     name: str = None
+    summary: str = None
+    description: str = None
+
+    def create_doc(self, namespace: str):
+        return remove_none({
+            "summary": self.summary, "description": self.description,
+            "tags": [{"name": f"namespace-{namespace}"}],
+            "message": {"$ref": f"#/components/messages/{self.model.__name__}"}
+        })
 
 
 @dataclass()
@@ -59,17 +72,42 @@ class EventGroup(Enum):
 
 
 class Namespace(_Namespace):
+    def __init__(self, namespace=None):
+        super().__init__(namespace)
+        self.doc_channels = {}
+        self.doc_messages = {}
+
     def attach_event_group(self, cls: Type[EventGroup]):
         for name, member in cls.__members__.items():
-            if isinstance(member, Event):
-                member.name = name.lower()
+            if not isinstance(member, Event):
+                continue
+
+            member.name = name.lower()
+            doc_data = {"description": ""}
+            event_doc = member.create_doc(self.namespace)
 
             if isinstance(member, ClientEvent):
                 if member.handler is None:
                     pass  # error!
                 setattr(self, f"on_{member.name}", member.handler)
-
-                pass  # documenting
+                doc_data["publish"] = event_doc
 
             if isinstance(member, ServerEvent):
-                pass  # documenting
+                doc_data["subscribe"] = event_doc
+
+            self.doc_channels[member.name] = doc_data
+            self.doc_messages[member.model.__name__] = {"payload": member.model.schema()}
+            # {"name": "", "title": "", "summary": "", "description": ""}
+
+
+class SocketIO(_SocketIO):
+    def __init__(self, title: str = "SIO", version: str = "1.0.0", doc_path: str = "/"):
+        self.async_api = {"asyncapi": "2.2.0", "info": {"title": title, "version": version},
+                          "channels": {}, "components": {"schemas": {}}}
+        self.doc_path = doc_path
+
+    def on_namespace(self, namespace_handler):
+        if isinstance(namespace_handler, Namespace):
+            self.async_api["channels"].update(namespace_handler.doc_channels)
+            self.async_api["components"]["schemas"].update(namespace_handler.doc_messages)
+        return super(SocketIO, self).on_namespace(namespace_handler)
