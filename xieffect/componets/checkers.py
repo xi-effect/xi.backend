336d48fa4c99622346cbaf2b32990eb3e6661ceb
qzart45hi <qwert45hi@yandex.ru>
2021-11-18 20:37:23 +0300

    fix: multiple sessions created for one request

--- xieffect/componets/checkers.py
+++ xieffect/componets/checkers.py
@@ -83,6 +83,8 @@ def with_session(function):
 
     @wraps(function)
     def with_session_inner(*args, **kwargs):
+        if "session" in kwargs.keys():
+            return function(*args, **kwargs)
         with Session.begin() as session:
             kwargs["session"] = session
             return function(*args, **kwargs)
