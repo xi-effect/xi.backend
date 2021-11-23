5efe0bb9613283ec3126b162d224f249a7bea5c4
sipmine <gleb.tedpoope@gmail.com>
2021-11-23 19:55:20 +0700

    feat: add invites.py

---
+++ xieffect/users/invites.py
@@ -0,0 +1,18 @@
+from flask_restx import Resource
+
+from componets import Namespace
+from users.database import User, Invite
+
+invites_namespace: Namespace = Namespace("invites", path="/invites/")
+
+
+@invites_namespace.route("/mine/")
+class Invite(Resource):
+    @invites_namespace.jwt_authorizer(User)
+    @invites_namespace.database_searcher(Invite)
+    @invites_namespace.a_response()
+    def get(self, session, user: User, invite: Invite):
+        if invite.find_by_id(session, user.invite_id) is None:
+            return 404
+        else:
+            return invite
