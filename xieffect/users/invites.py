a0b8780c09d0fb6819b58c9a24812bb73feb7f17
sipmine <gleb.tedpoope@gmail.com>
2021-11-24 21:59:55 +0700

    fix: invites.py

--- xieffect/users/invites.py
+++ xieffect/users/invites.py
@@ -1,17 +1,19 @@
 from flask_restx import Resource
 
 from componets import Namespace
-from users.database import User, Invite
+from .database import User, Invite
 
 invites_namespace: Namespace = Namespace("invites", path="/invites/")
+invites_model = invites_namespace.model("Invite", Invite.marshal_models["invite"])
 
 
 @invites_namespace.route("/mine/")
-class Invite(Resource):
+class InviteGetter(Resource):
     @invites_namespace.jwt_authorizer(User)
-    @invites_namespace.a_response()
-    def get(self, session, user: User, invite: Invite):
-        if self.find_by_id(session, user.invite_id) is None:
-            return 404
+    @invites_namespace.marshal_with(invites_model)
+    def get(self, session, user: User):
+        invite = Invite.find_by_id(session, user.invite_id)
+        if invite is None:
+            return {"a": "Invite not found"}, 404
         else:
             return invite
