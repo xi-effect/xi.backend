8c23b060d1f1cc65be9a656b0860d75f3e575d5f
qzart45hi <qwert45hi@yandex.ru>
2022-01-15 02:09:56 +0300

    feat: merge _other.py into _utils.py

--- xieffect/common/__init__.py
+++ xieffect/common/__init__.py
@@ -2,10 +2,9 @@ from ._restx import UserRole, Identifiable, Namespace
 from ._eventor import error_event, Namespace as SIONamespace, users_broadcast
 from ._marshals import LambdaFieldDef, Marshalable, ResponseDoc
 from ._marshals import create_marshal_model, unite_models, message_response, success_response, DateTimeField
-from ._other import TypeEnum
 from ._parsers import counter_parser, password_parser
 from ._sqlalchemy import with_session, with_auto_session, register_as_searchable
-from ._utils import get_or_pop
+from ._utils import TypeEnum, get_or_pop
 from ._whoosh import IndexService, Searcher
 from .flask_siox import ClientEvent, ServerEvent, DuplexEvent, SocketIO, EventGroup
 from .users_db import User, TokenBlockList
