fb5065353b73b77402ecc03870e238f489fb46b0
sipmine <gleb.tedpoope@gmail.com>
2022-01-18 23:39:25 +0700

    feat: add fields on db TestResult

--- xieffect/education/knowledge/results_db.py
+++ xieffect/education/knowledge/results_db.py
@@ -6,18 +6,32 @@ from sqlalchemy import Column, ForeignKey, select
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import Integer, String, Boolean, JSON, DateTime, Text, Enum
 
-from common import Identifiable, Marshalable, LambdaFieldDef, create_marshal_model, register_as_searchable, TypeEnum
+from common import Identifiable, Marshalable, LambdaFieldDef, create_marshal_model, register_as_searchable, TypeEnum, User
+from education.knowledge.interaction_db import TestModuleSession
 from main import Base, Session
 
 
+@create_marshal_model("TestResult", "short_result", "result")
 class TestResult(Base):
-    # id PK
-    # user_id FK
-    # module_id FK
-    # short_result (JSON)
-    # result (JSON)
+    __tablename__ = "TestResult"
 
-    # create
-    # find_by_id
-    # find_by_user
-    pass
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    module_id = Column(Integer, ForeignKey("module.id"), nullable=False)
+    short_result = Column(JSON)
+    result = Column(JSON)
+
+    @classmethod
+    def create(cls, session: Session, user_id: int, module_id: int) -> TestResult:
+        new_entry = cls(user_id=user_id, module_id=module_id)
+        session.add(new_entry)
+        session.flush()
+        return new_entry
+
+    @classmethod
+    def find_by_id(cls, session: Session, entry_id: int) -> Union[TestModuleSession, None]:
+        return session.execute(select(cls).where(cls.id == entry_id)).scalars().first()
+
+    @classmethod
+    def find_by_user(cls, session: Session, user_id: int, offset: int, limit: int) -> list[User]:
+        return session.execute(select(cls).filter_by(user_id=user_id).offset(offset).limit(limit)).scalars().all()
