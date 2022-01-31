08bf2afd56d47292c10a8e0de8e5c0237f35a148
sipmine <gleb.tedpoope@gmail.com>
2022-01-31 20:59:27 +0700

    feat: pagination all result

--- xieffect/education/knowledge/results_rst.py
+++ xieffect/education/knowledge/results_rst.py
@@ -1,30 +1,34 @@
 from flask_restx import Resource, fields, Model
 
-from common import Namespace, User
+from common import Namespace, User, counter_parser
 from .results_db import TestResult
 
-result_namespace: Namespace = Namespace("result", path='/modules/<int:module_id>/result/')
+result_namespace: Namespace = Namespace("result", path="/modules/<int:module_id>/result/")
 result_dict = {
-    'right-answers': fields.Integer,
-    'total-answers': fields.Integer,
-    'percent': fields.Integer,
+    "right-answers": fields.Integer,
+    "total-answers": fields.Integer,
+    "page-id": fields.Integer,
+    "point-id": fields.Integer,
+    "answers": fields.Raw
 }
-# result_dict['percent'] = (result_dict['total-answers'] / 100) * result_dict['right-answers']
-result_model = Model('ResultModel', {
-    'module-name': fields.String,
-    'author-name': fields.String,
-    'author-id': fields.Integer,
-    'result': fields.List(fields.Nested(result_dict))
+# result_dict["percent"] = (result_dict["total-answers"] / 100) * result_dict["right-answers"]
+result_model = Model("ResultModel", {
+    "module-name": fields.String,
+    "author-name": fields.String,
+    "author-id": fields.Integer,
+    "result": fields.List(fields.Nested(result_dict))
 })
 
 
-# TODO /modules/<module_id>/results/ -> пагинация, минимальная информация
-# TODO использовать user из авторизации
-# @result_namespace.route("/modules/<int:module_id>/result/")
-# class PagesResult(Resource):
-#     @result_namespace.lister(10):
+@result_namespace.route("/")
+class PagesResult(Resource):
+    @result_namespace.jwt_authorizer(User)
+    @result_namespace.argument_parser(counter_parser)
+    @result_namespace.lister(50, result_model)
+    def post(self, session, module_id: int, user: User, start: int, finish: int):
+        return TestResult.find_by_module(session, user.id, module_id, start, finish - start)
 
-# TODO /modules/<module_id>/results/<result_id>/ -> GET & DELETE
+
 @result_namespace.route("/<int:result_id>/")
 class Result(Resource):
     @result_namespace.jwt_authorizer(User, use_session=True)
@@ -37,6 +41,3 @@ class Result(Resource):
     def delete(self, session, result_id, user: User, module_id: int) -> None:
         entry: TestResult = TestResult.find_by_id(session, result_id)
         session.delete(entry)
-
-
-m
