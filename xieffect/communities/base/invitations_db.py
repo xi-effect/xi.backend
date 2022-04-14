a3d351d428e7d4fefb62e7f69c76c99660578350
qzart45hi <qwert45hi@yandex.ru>
2022-04-14 03:06:54 +0300

    feat: redo cs-invitaions

--- xieffect/communities/base/invitations_db.py
+++ xieffect/communities/base/invitations_db.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Union
+from datetime import datetime, timedelta
 
 from itsdangerous import URLSafeSerializer
 from sqlalchemy import Column, select, ForeignKey
@@ -11,51 +11,45 @@ from common import Marshalable, create_marshal_model, Identifiable, Base, sessio
 from .meta_db import Community, ParticipantRole
 
 
-@create_marshal_model("invitation-base", "community_id", "time_limit", "code", "role", "count_limit")
+@create_marshal_model("invitation-index", "role", "deadline", "limit", inherit="invitation-base")
+@create_marshal_model("invitation-base", "id", "code")
 class Invitation(Base, Identifiable, Marshalable):
     __tablename__ = "community_invites"
     serializer: URLSafeSerializer = URLSafeSerializer(app.config["SECURITY_PASSWORD_SALT"])
 
-    invitation_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    code = Column(String(100), default="")
 
     community_id = Column(Integer, ForeignKey(Community.id), nullable=False)
     community = relationship("Community")
 
-    time_limit = Column(DateTime, nullable=True)
-    code = Column(String(100), default="")
-    role = Column(Enum(ParticipantRole), default=1)
-    count_limit = Column(Integer, nullable=False, default=-1)
+    role = Column(Enum(ParticipantRole), nullable=False)
+    deadline = Column(DateTime, nullable=True)
+    limit = Column(Integer, nullable=True)
 
     @classmethod
-    def create(cls, session: sessionmaker, community_id: int, role: ParticipantRole, count_limit: int = -1,
-               time_limit: Union[DateTime, None] = None) -> Invitation:
-        entry: cls = super().create(session, role=role, count_limit=count_limit, time_limit=time_limit,
-                                    community=Community.find_by_id(session, community_id))
+    def create(cls, session: sessionmaker, community_id: int, role: ParticipantRole, limit: int | None,
+               days_to_live: int | None) -> Invitation:
+        entry: cls = super().create(session, role=role, community_id=community_id, limit=limit,
+                                    deadline=datetime.utcnow() + timedelta(days=days_to_live))
         entry.code = entry.generate_code()
         session.flush()
         return entry
 
     @classmethod
     def find_by_id(cls, session: sessionmaker, invitation_id: int) -> Invitation | None:
-        return session.get_first(select(cls).filter_by(invitation_id=invitation_id))
+        return session.get_first(select(cls).filter_by(id=invitation_id))
 
     @classmethod
-    def find_by_community_id(cls, session: sessionmaker, community_id: int, offset: int, limit: int) -> list[Invitation]:
-        return session.get_paginated(select(cls).filter(cls.community_id == community_id), offset, limit)
+    def find_by_community(cls, session: sessionmaker, community_id: int, offset: int, limit: int) -> list[Invitation]:
+        return session.get_paginated(select(cls).filter_by(community_id=community_id), offset, limit)
 
     @classmethod
-    def find_by_code(cls, session: sessionmaker, code: str):
+    def find_by_code(cls, session: sessionmaker, code: str) -> Invitation | None:
         return session.get_first(select(cls).filter_by(code=code))
 
     def generate_code(self):
         return self.serializer.dumps((self.community_id, self.invitation_id))
 
-    def check_availabel(self, date: DateTime):
-        if self.count_limit != 0 and self.time_limit > date:
-            return True
-        else:
-            return False
-
-    def delete(self, session):
-        session.delete(self)
-        session.flush()
+    def is_available(self, date: DateTime):
+        return self.count_limit != 0 and self.time_limit > date
