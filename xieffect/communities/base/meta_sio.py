17f5de7b14bb72fe356f2b08b90ec2d90aade242
qzart45hi <qwert45hi@yandex.ru>
2022-05-02 15:20:47 +0300

    feat: communities list database & events

--- xieffect/communities/base/meta_sio.py
+++ xieffect/communities/base/meta_sio.py
@@ -1,12 +1,48 @@
+from pydantic import BaseModel, Field
+
 from common import EventGroup, EventSpace, DuplexEvent, User
 from .meta_db import Community
+from .users_ext_db import CommunitiesUser
 
 communities_meta_events = EventGroup(use_kebab_case=True)
 
 
 class CommunitiesEventSpace(EventSpace):
+    new_community = communities_meta_events.bind_sub(Community.IndexModel)
+
     @communities_meta_events.bind_dup(Community.CreateModel, Community.BaseModel, use_event=True)
     @communities_meta_events.jwt_authorizer(User)
     def create_community(self, event: DuplexEvent, session, user: User, name: str, description: str = None):
         community = Community.create(session, name, description, user)
+        cu = CommunitiesUser.find_or_create(session, user.id)
+        cu.join_community(community.id)
+
+        CommunitiesEventSpace.new_community.emit(_data=community, _room=f"user-{user.id}", _include_self=False)
+        # TODO fix in ffs  # TODO binding should be done on the whole EventSpace via metaclass
         event.emit(_data=community)
+
+    class CommunityIdModel(BaseModel):
+        community_id: int
+
+    @communities_meta_events.bind_dup(CommunityIdModel, use_event=True)
+    @communities_meta_events.jwt_authorizer(User)
+    @communities_meta_events.database_searcher(Community, use_session=True)
+    def leave_community(self, event: DuplexEvent, session, user: User, community: Community):
+        cu = CommunitiesUser.find_or_create(session, user.id)
+        cu.leave_community(session, community.id)
+        event.emit(community_id=community.id, _room=f"user-{user.id}", _include_self=False)
+
+    class ReorderModel(BaseModel):
+        community_id: int = Field(alias="source-id")
+        target_index: int
+
+    @communities_meta_events.bind_dup(ReorderModel, use_event=True)
+    @communities_meta_events.jwt_authorizer(User)
+    @communities_meta_events.database_searcher(Community, use_session=True)
+    def reorder_community(self, event: DuplexEvent, session, user: User, community: Community, target_index: int):
+        cu = CommunitiesUser.find_or_create(session, user.id)
+        if cu.reorder_community_list(session, community.id, target_index):
+            event.emit(community_id=community.id, target_index=target_index,
+                       _room=f"user-{user.id}", _include_self=False)
+        else:
+            communities_meta_events.abort(404, "Community not in the list")
---
+++ xieffect/communities/base/users_ext_db.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+
+from sqlalchemy import Column, ForeignKey, select
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean, JSON, DateTime, Text, Enum
+from sqlalchemy.ext.orderinglist import ordering_list
+
+from common import Identifiable, PydanticModel, TypeEnum, Base, sessionmaker
+from communities.base.meta_db import Community
+
+
+class CommunitiesUser(Base):
+    __tablename__ = "communities_users"
+
+    id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+
+    communities = relationship("CommunityListItem", order_by="CommunityListItem.position",
+                               collection_class=ordering_list("position"))
+
+    class FullModel(PydanticModel):
+        communities: list[Community.IndexModel]
+
+        @classmethod
+        def callback_convert(cls, callback: Callable, orm_object: CommunitiesUser, **context) -> None:
+            callback(communities=[Community.IndexModel.convert(ci.community, **context)
+                                  for ci in orm_object.communities])
+
+    @classmethod
+    def _create_empty(cls, session, user_id: int) -> CommunitiesUser:
+        return cls.create(session, id=user_id)
+
+    @classmethod
+    def find_by_id(cls, session: sessionmaker, user_id: int) -> CommunitiesUser | None:
+        return session.get_first(select(cls).filter_by(id=user_id))
+
+    @classmethod
+    def find_or_create(cls, session: sessionmaker, user_id: int) -> CommunitiesUser:
+        return cls.find_by_id(session, user_id) or cls._create_empty(session, user_id)
+
+    def reorder_community_list(self, session, source_id: int, target_index: int) -> bool:
+        list_item = CommunityListItem.find_by_community(session, source_id)
+        if list_item is None:
+            return False
+        self.communities.remove(list_item)
+        self.communities.insert(target_index, list_item)
+        return True
+
+    def join_community(self, community_id: int) -> None:  # autocommit  # TODO find a way to reverse the list
+        new_item = CommunityListItem(community_id=community_id)
+        self.communities.insert(0, new_item)
+
+    def leave_community(self, session, community_id: int) -> bool:
+        list_item = CommunityListItem.find_by_community(session, community_id)
+        if list_item is None:
+            return False
+        self.communities.remove(list_item)
+        return True
+
+
+class CommunityListItem(Base):
+    __tablename__ = "community_lists"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("communities_users.id"))
+    position = Column(Integer)
+
+    community_id = Column(Integer, ForeignKey("community.id"), nullable=False, unique=True)
+    community = relationship("Community")
+
+    @classmethod
+    def find_by_community(cls, session, community_id: int) -> CommunityListItem | None:
+        return session.get_first(select(cls).filter_by(id=community_id))
