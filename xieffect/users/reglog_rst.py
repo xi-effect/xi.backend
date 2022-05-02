5c2477789cb1f35e99c02008885ac8dc2c5b0c20
qzart45hi <qwert45hi@yandex.ru>
2022-05-02 23:20:54 +0300

    feat: new user info model & /home/ endpoint

--- xieffect/users/reglog_rst.py
+++ xieffect/users/reglog_rst.py
@@ -1,19 +1,27 @@
-from flask import jsonify
-from flask_jwt_extended import get_jwt, unset_jwt_cookies
+from flask_jwt_extended import get_jwt
 from flask_restx import Resource
 from flask_restx.reqparse import RequestParser
 from itsdangerous import BadSignature
 
 from common import password_parser, Namespace, success_response, TokenBlockList, User
+from communities import CommunitiesUser
 from .invites_db import Invite
 # from .emailer import send_generated_email, parse_code
 
 reglog_namespace: Namespace = Namespace("reglog", path="/")
 success_response.register_model(reglog_namespace)
-add_sets_cookie_response = reglog_namespace.response(*success_response.get_args(),
-                                                     headers={"SetCookie": "sets access_token_cookie"})
-add_unsets_cookie_response = reglog_namespace.response(*success_response.get_args(),
-                                                       headers={"SetCookie": "unsets access_token_cookie"})
+# add_sets_cookie_response = reglog_namespace.response(*success_response.get_args(),  # TODO use this is ffs
+#                                                      headers={"SetCookie": "sets access_token_cookie"})
+# add_unsets_cookie_response = reglog_namespace.response(*success_response.get_args(),
+#                                                        headers={"SetCookie": "unsets access_token_cookie"})
+
+
+@reglog_namespace.route("/home/")
+class UserHome(Resource):
+    @reglog_namespace.jwt_authorizer(User)
+    @reglog_namespace.marshal_with(CommunitiesUser.FullModel)
+    def get(self, session, user: User):
+        return CommunitiesUser.find_or_create(session, user.id)
 
 
 @reglog_namespace.route("/reg/")
@@ -23,10 +31,9 @@ class UserRegistration(Resource):
     parser.add_argument("username", required=True, help="Username to be assigned to new user's account")
     parser.add_argument("code", required=True, help="Serialized invite code")
 
-    @add_sets_cookie_response
     @reglog_namespace.with_begin
     @reglog_namespace.argument_parser(parser)
-    @reglog_namespace.adds_authorization()
+    @reglog_namespace.marshal_with_authorization(CommunitiesUser.FullModel)
     def post(self, session, email: str, username: str, password: str, code: str):
         """ Creates a new user if email is not used already, logs in automatically """
         try:
@@ -43,7 +50,8 @@ class UserRegistration(Resource):
         if (user := User.create(session, email=email, username=username, password=password, invite=invite)) is None:
             return {"a": "Email already in use"}
         # send_generated_email(email, "confirm", "registration-email.html")
-        return {"a": "Success"}, user
+        cu = CommunitiesUser.find_or_create(session, user.id)
+        return cu, user
 
 
 @reglog_namespace.route("/auth/")
@@ -51,44 +59,42 @@ class UserLogin(Resource):
     parser: RequestParser = password_parser.copy()
     parser.add_argument("email", required=True, help="User's email")
 
-    @add_sets_cookie_response
     @reglog_namespace.with_begin
     @reglog_namespace.argument_parser(parser)
-    @reglog_namespace.adds_authorization()
+    @reglog_namespace.marshal_with_authorization(CommunitiesUser.FullModel)
     def post(self, session, email: str, password: str):
         """ Tries to log in with credentials given """
         if (user := User.find_by_email_address(session, email)) is None:
             return {"a": "User doesn't exist"}
 
         if User.verify_hash(password, user.password):
-            return {"a": "Success"}, user
+            cu = CommunitiesUser.find_or_create(session, user.id)
+            return cu, user
         return {"a": "Wrong password"}
-
-
-@reglog_namespace.route("/logout/")
-class UserLogout(Resource):
-    @add_unsets_cookie_response
-    @reglog_namespace.with_begin
-    @reglog_namespace.removes_authorization()
-    def post(self, session):
-        """ Logs the user out, blocks the token """
-        TokenBlockList.create(session, jti=get_jwt()["jti"])
-        return {"a": True}
 
 
 @reglog_namespace.route("/go/")
 class Test(Resource):
     from api import app
 
-    @add_sets_cookie_response
     @reglog_namespace.with_begin
-    @reglog_namespace.adds_authorization()
+    @reglog_namespace.marshal_with_authorization(CommunitiesUser.FullModel)
     def get(self, session):
         """ Localhost-only endpoint for logging in from the docs """
         if not self.app.debug:
             return {"a": False}
 
-        return {"a": True}, User.find_by_id(session, 1)
+        return CommunitiesUser.find_or_create(session, 1), User.find_by_id(session, 1)
+
+
+@reglog_namespace.route("/logout/")
+class UserLogout(Resource):
+    @reglog_namespace.with_begin
+    @reglog_namespace.removes_authorization()
+    def post(self, session):
+        """ Logs the user out, blocks the token """
+        TokenBlockList.create(session, jti=get_jwt()["jti"])
+        return {"a": True}
 
 
 @reglog_namespace.route("/password-reset/")
