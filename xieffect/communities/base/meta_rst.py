7ac83330c374d527ed9a7f2253569acac954608b
qzart45hi <qwert45hi@yandex.ru>
2022-07-20 01:05:14 +0300

    feat: temp community reader request

--- xieffect/communities/base/meta_rst.py
+++ xieffect/communities/base/meta_rst.py
@@ -4,7 +4,7 @@ from flask_restx import Resource
 from flask_restx.reqparse import RequestParser
 
 from common import ResourceController, counter_parser, User
-from .meta_db import Community
+from .meta_db import Community, Participant
 
 controller = ResourceController("communities-meta", path="/communities/")
 
@@ -31,3 +31,15 @@ class CommunityLister(Resource):
     @controller.lister(20, Community.IndexModel)
     def post(self, session, user: User, start: int, finish: int):
         return Community.find_by_user(session, user, start, finish - start)
+
+
+@controller.route("/<int:community_id>/")
+class CommunityReader(Resource):
+    @controller.doc_abort(403, "Not a member")
+    @controller.jwt_authorizer(User)
+    @controller.database_searcher(Community, use_session=True)
+    @controller.marshal_with(Community.IndexModel)
+    def post(self, session, user, community: Community):
+        if Participant.find_by_ids(session, community.id, user.id) is None:
+            controller.abort(403, "Not a member")
+        return community
