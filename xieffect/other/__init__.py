6deff929b56e530944cb977ca58027044c07bf44
qzart45hi <qwert45hi@yandex.ru>
2022-07-24 22:26:32 +0300

    feat: create_email_confirmer function

--- xieffect/other/__init__.py
+++ xieffect/other/__init__.py
@@ -1,4 +1,4 @@
 from .discorder import send_file_message as send_file_discord_message
 from .discorder import send_message as send_discord_message, WebhookURLs
-from .emailer import EmailType, send_email, send_code_email
+from .emailer import EmailType, send_email, send_code_email, create_email_confirmer
 from .updater_rst import controller as webhook_namespace
