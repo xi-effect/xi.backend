2ea0f4ffa26d1d1b2a471f2a31fc8bb272beb528
qzart45hi <qwert45hi@yandex.ru>
2022-08-17 00:28:43 +0300

    feat(vault): rest api & connections

--- xieffect/api.py
+++ xieffect/api.py
@@ -13,6 +13,7 @@ from moderation import (mub_base_namespace, mub_cli_blueprint, mub_super_namespa
 from other import (webhook_namespace, send_discord_message, send_file_discord_message, WebhookURLs)
 from users import (reglog_namespace, users_namespace, invites_namespace, feedback_namespace,
                    settings_namespace, users_mub_namespace, emailer_qa_namespace)
+from vault import files_namespace
 
 logger = Logger("flask-fullstack", "WARN")
 
@@ -36,6 +37,8 @@ def log_stuff(level: str, message: str):
 jwt = app.configure_jwt_with_loaders(["cookies"], timedelta(hours=72), lambda *x: logger.warning(x[1]),
                                      samesite_cookie="None", csrf_protect=False)
 api = app.configure_restx()
+
+api.add_namespace(files_namespace)
 
 api.add_namespace(reglog_namespace)
 api.add_namespace(users_namespace)
