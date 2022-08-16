7e50ed26e796e7e70d739c1a8775d3793ca03b95
qzart45hi <qwert45hi@yandex.ru>
2022-08-16 22:21:53 +0300

    refactor: cleanup unneeded comments

--- xieffect/common/_eventor.py
+++ xieffect/common/_eventor.py
@@ -3,13 +3,6 @@ from flask import Flask
 from __lib__.flask_fullstack import EventController as _EventController, PydanticModel, SocketIO as _SocketIO
 
 
-# DEPRECATED due to support for acknowledgements
-# @dataclass
-# class EventException(Exception):
-#     code: int
-#     message: str
-
-
 class EventController(_EventController):
     from ._core import sessionmaker
 
@@ -17,28 +10,6 @@ class EventController(_EventController):
         kwargs["sessionmaker"] = kwargs.get("sessionmaker", self.sessionmaker)
         kwargs["use_kebab_case"] = kwargs.get("use_kebab_case", True)
         super().__init__(*args, **kwargs)
-
-    # DEPRECATED due to support for acknowledgements
-    # def triggers(self, event: ServerEvent, condition: str = None, data: ... = None):
-    #     def triggers_wrapper(function):
-    #         if not hasattr(function, "__sio_doc__"):
-    #             setattr(function, "__sio_doc__", {"x-triggers": []})
-    #         if (x_triggers := function.__sio_doc__.get("x-triggers", None)) is None:
-    #             function.__sio_doc__["x-triggers"] = []
-    #             x_triggers = function.__sio_doc__["x-triggers"]
-    #         x_triggers.append({
-    #             "event": event.name,
-    #             "condition": condition,
-    #             "data": data
-    #         })
-    #         return function
-
-    #    return triggers_wrapper
-
-    # T/O/D/O use .triggers() for bind_pub with use_event (when ffs will support `x-triggers` correctly)
-
-    # def doc_abort(self, error_code: Union[int, str], description: str, *, critical: bool = False):
-    #    return self.triggers(error_event, description, {"code": error_code})
 
 
 class EmptyBody(PydanticModel):
