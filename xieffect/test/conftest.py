cfc933d389d54625ea5c3bfb0e27e2bdc6077efe
qzart45hi <qwert45hi@yandex.ru>
2022-08-17 17:53:50 +0300

    feat(testing): test_files_normal

--- xieffect/test/conftest.py
+++ xieffect/test/conftest.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Iterator, Callable
+from typing import Tuple, Iterator, Callable, Protocol
 
 from flask.testing import FlaskClient
 from flask_socketio import SocketIOTestClient
@@ -7,7 +7,7 @@ from werkzeug.test import TestResponse
 
 from __lib__.flask_fullstack import check_code
 from api import socketio
-from wsgi import application as app, TEST_EMAIL, BASIC_PASS, ADMIN_EMAIL, ADMIN_PASS
+from wsgi import application as app, TEST_EMAIL, BASIC_PASS, ADMIN_EMAIL, ADMIN_PASS, TEST_MOD_NAME, TEST_PASS
 
 
 # from .library2 import MultiClient as _MultiClient, DoubleClient, SocketIOTestClient
@@ -29,21 +29,37 @@ def base_client():
         yield client
 
 
-def login(email: str, password: str) -> FlaskClient:
+def base_login(client: FlaskClient, account: str, password: str, mub: bool = False) -> None:
+    response: TestResponse = client.post("/mub/sign-in/" if mub else "/auth/",
+                                         data={"username" if mub else "email": account, "password": password})
+    assert response.status_code == 200
+    assert "Set-Cookie" in response.headers.keys()
+    cookie: Tuple[str, str] = response.headers["Set-Cookie"].partition("=")[::2]
+    assert cookie[0] == "access_token_cookie"
+    client.set_cookie("test", "access_token_cookie", cookie[1])
+
+
+def login(account: str, password: str, mub: bool = False) -> FlaskClient:
     with app.test_client() as client:
-        response: TestResponse = client.post("/auth/", follow_redirects=True,
-                                             data={"email": email, "password": password})
-        assert response.status_code == 200
-        assert "Set-Cookie" in response.headers.keys()
-        cookie: Tuple[str, str] = response.headers["Set-Cookie"].partition("=")[::2]
-        assert cookie[0] == "access_token_cookie"
-        client.set_cookie("test", "access_token_cookie", cookie[1])
+        base_login(client, account, password, mub)
         return client
 
 
 @fixture
 def client() -> FlaskClient:
     return login(TEST_EMAIL, BASIC_PASS)
+
+
+@fixture
+def mod_client() -> FlaskClient:
+    return login(TEST_MOD_NAME, TEST_PASS, True)
+
+
+@fixture
+def full_client() -> FlaskClient:
+    client = login(TEST_EMAIL, BASIC_PASS)
+    base_login(client, TEST_MOD_NAME, TEST_PASS, True)
+    return client
 
 
 def socketio_client_factory(client: FlaskClient) -> SocketIOTestClient:
@@ -68,14 +84,19 @@ def multi_client(base_client: FlaskClient) -> Callable[[str], FlaskClient]:
     return multi_client_inner
 
 
+class ListTesterProtocol(Protocol):
+    def __call__(self, link: str, request_json: dict, page_size: int, status_code: int = 200, /) -> Iterator[dict]:
+        pass
+
+
 @fixture
-def list_tester(client: FlaskClient) -> Callable[[str, dict, int, int], Iterator[dict]]:
+def list_tester(full_client: FlaskClient) -> ListTesterProtocol:
     def list_tester_inner(link: str, request_json: dict, page_size: int, status_code: int = 200) -> Iterator[dict]:
         counter = 0
         amount = page_size
         while amount == page_size:
             request_json["counter"] = counter
-            response_json: dict = check_code(client.post(link, json=request_json), status_code)
+            response_json: dict = check_code(full_client.post(link, json=request_json), status_code)
 
             assert "results" in response_json
             assert isinstance(response_json["results"], list)
