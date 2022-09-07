fdee7ca6b2c8c60f86bd188c3981fd8d84a621cc
porebrikk <jam.d456@gmail.com>
2022-09-07 22:10:31 +1000

    fix: db's methods, parsers & endpoints

--- xieffect/communities/services/news_db.py
+++ xieffect/communities/services/news_db.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from flask_restx import inputs
-
 from sqlalchemy import Column, select, ForeignKey, sql
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.sql.sqltypes import Integer, String, Boolean, Text, DateTime
@@ -11,8 +9,8 @@ from common import Base, sessionmaker, User
 from communities.base.meta_db import Community
 
 
-class News(Base, Identifiable):
-    __tablename__ = "news"
+class Post(Base, Identifiable):
+    __tablename__ = "cs_posts"
 
     # Vital
     id = Column(Integer, primary_key=True)
@@ -24,31 +22,29 @@ class News(Base, Identifiable):
 
     # User-related
     user_id = Column(Integer, ForeignKey(User.id), nullable=False)
-    user = relationship("User", backref='news')
+    user = relationship("User", backref='posts')
 
     # Community-related
     community_id = Column(Integer, ForeignKey(Community.id), nullable=False)
-    community = relationship("Community", backref=backref("News", cascade="all, delete, delete-orphan"))
+    community = relationship("Community", backref=backref("Post", cascade="all, delete, delete-orphan"))
 
     MainData = PydanticModel.column_model(id, title, description, create_datetime, change_datetime, deleted,
                                           user_id, community_id)
 
     # Find a paginated list of community news
     @classmethod
-    def find_by_community(cls, session: sessionmaker, community_id: int, offset: int, limit: int) -> list[News] | None:
+    def find_by_community(cls, session: sessionmaker, community_id: int, offset: int, limit: int) -> list[Post] | None:
         return session.get_paginated(select(cls).filter_by(community_id=community_id), offset, limit)
 
     # Create news
     @classmethod
-    def create(cls, session: sessionmaker, title: str, description: str | None, create_datetime: inputs.datetime,
-               change_datetime: inputs.datetime, deleted: bool, user_id: int, community_id: int) -> News:
-        entry: cls = super().create(session, title=title, description=description, create_datetime=create_datetime,
-                                    change_datetime=change_datetime, deleted=deleted, user_id=user_id,
+    def create(cls, session: sessionmaker, title: str, description: str | None, user_id: int,
+               community_id: int) -> Post:
+        entry: cls = super().create(session, title=title, description=description, user_id=user_id,
                                     community_id=community_id)
-        session.flush()
         return entry
 
     # Find list of community news by id
     @classmethod
-    def find_by_id(cls, session: sessionmaker, community_id: int, entry_id: int) -> News | None:
-        return session.get_first(select(cls).filter_by(community_id=community_id, id=entry_id))
+    def find_by_id(cls, session: sessionmaker, entry_id: int) -> Post | None:
+        return session.get_first(select(cls).filter_by(id=entry_id))
--- xieffect/communities/services/news_rst.py
+++ xieffect/communities/services/news_rst.py
@@ -2,105 +2,90 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from flask_restx import Resource, inputs
+from flask_restx import Resource
 from flask_restx.reqparse import RequestParser
 
-from sqlalchemy import select
-
 from common import ResourceController, User, counter_parser
-from .news_db import News
+from .news_db import Post
 from communities.base.meta_db import Community, Participant, ParticipantRole
 
 controller = ResourceController("communities-news", path="/communities/")
 
+# Parser for create and update news
+news_parser: RequestParser = RequestParser()
+news_parser.add_argument("title", type=str, required=True)
+news_parser.add_argument("description", type=str)
+
 
 @controller.route("/<int:community_id>/news/index/")
 class NewsLister(Resource):
-    # Parser for create news
-    news_post_parser: RequestParser = RequestParser()
-    news_post_parser.add_argument("title", type=str, required=True)
-    news_post_parser.add_argument("description", type=str)
-    news_post_parser.add_argument("create_datetime", type=inputs.datetime)
-    news_post_parser.add_argument("change_datetime", type=inputs.datetime)
-    news_post_parser.add_argument("deleted", type=bool)
-
     # Get list of news with pagination
     @controller.jwt_authorizer(User, check_only=True)
     @controller.argument_parser(counter_parser)
     @controller.database_searcher(Community, check_only=True, use_session=True)
-    @controller.lister(20, News.MainData)
+    @controller.lister(20, Post.MainData)
     def get(self, session, community_id: int, start: int, finish: int):
-        return News.find_by_community(session, community_id, start, finish - start)
+        return Post.find_by_community(session, community_id, start, finish - start)
 
     # Create news
-    @controller.doc_abort(400, "Participant does not exist")
-    @controller.doc_abort(403, "Must have the rights of the OWNER")
+    @controller.doc_abort(403, "Permission Denied")
     @controller.jwt_authorizer(User)
-    @controller.argument_parser(news_post_parser)
+    @controller.argument_parser(news_parser)
     @controller.database_searcher(Community, check_only=True, use_session=True)
-    @controller.marshal_with(News.MainData)
-    def post(self, session, title: str, description: str, create_datetime: inputs.datetime,
-             change_datetime: inputs.datetime, deleted: bool, user: User, community_id: int):
-        user_id = user.id
+    @controller.marshal_with(Post.MainData)
+    def post(self, session, title: str, description: str, user: User, community_id: int):
         # Community membership check
-        if (participant := session.get_first(select(Participant).filter_by(user_id=user_id))) is None:
-            return controller.abort(400, "Participant does not exist")
+        if (participant := Participant.find_by_ids(session, community_id, user.id)) is None:
+            return controller.abort(403, "Permission Denied: Participant not found")
         # Participant role check
         if participant.role == ParticipantRole.OWNER:
-            return News.create(session, title, description, create_datetime, change_datetime, deleted,
-                               user_id, community_id)
+            return Post.create(session, title, description, user.id, community_id)
         else:
-            return controller.abort(403, "Must have the rights of the OWNER")
+            return controller.abort(403, "Permission Denied: Low role")
 
 
 @controller.route("/<int:community_id>/news/<int:entry_id>/")
 class NewsChanger(Resource):
-    # Parser for update news
-    news_change_parser: RequestParser = RequestParser()
-    news_change_parser.add_argument("title", type=str, required=True)
-    news_change_parser.add_argument("description", type=str)
+    # Create title optionally for update news
+    news_parser.replace_argument("title", type=str, required=False)
 
     # Update news
-    @controller.doc_abort(404, "News does not exist")
-    @controller.doc_abort(400, "Participant does not exist")
-    @controller.doc_abort(403, "Must have the rights of the OWNER")
+    @controller.doc_abort(404, "News not found")
+    @controller.doc_abort(403, "Permission Denied")
     @controller.jwt_authorizer(User)
-    @controller.argument_parser(news_change_parser)
+    @controller.argument_parser(news_parser)
     @controller.database_searcher(Community, check_only=True, use_session=True)
-    @controller.marshal_with(News.MainData)
+    @controller.marshal_with(Post.MainData)
     def put(self, session, title: str, description: int, user: User, community_id: int, entry_id: int):
-        user_id = user.id
         # News availability check
-        if (update_news := News.find_by_id(session, community_id, entry_id)) is None:
-            return controller.abort(404, "News does not exist")
+        if (update_news := Post.find_by_id(session, entry_id)) is None or update_news.community_id != community_id:
+            return controller.abort(404, "News not found")
         # Community membership check
-        if (participant := session.get_first(select(Participant).filter_by(user_id=user_id))) is None:
-            return controller.abort(400, "Participant does not exist")
+        if (participant := Participant.find_by_ids(session, community_id, user.id)) is None:
+            return controller.abort(403, "Permission Denied: Participant not found")
         # Participant role check
         if participant.role == ParticipantRole.OWNER:
-            update_news.title = update_news.title if (title is None) else title
-            update_news.description = update_news.description if (description is None) else description
+            update_news.title = update_news.title if title is None else title
+            update_news.description = update_news.description if description is None else description
             update_news.change_datetime = datetime.utcnow().replace(microsecond=0)
             return update_news
         else:
-            return controller.abort(403, "Must have the rights of the OWNER")
+            return controller.abort(403, "Permission Denied: Low role")
 
     # Soft-delete news
-    @controller.doc_abort(404, "News does not exist")
-    @controller.doc_abort(400, "Participant does not exist")
-    @controller.doc_abort(403, "Must have the rights of the OWNER")
+    @controller.doc_abort(404, "News not found")
+    @controller.doc_abort(403, "Permission Denied")
     @controller.jwt_authorizer(User)
     def delete(self, session, community_id: int, entry_id: int, user: User):
-        user_id = user.id
         # Deleted news availability check
-        if (deleted_news := News.find_by_id(session, community_id, entry_id)) is None:
-            return controller.abort(404, "News does not exist")
+        if (deleted_news := Post.find_by_id(session, entry_id)) is None or deleted_news.community_id != community_id:
+            return controller.abort(404, "News not found")
         # Community membership check
-        if (participant := session.get_first(select(Participant).filter_by(user_id=user_id))) is None:
-            return controller.abort(400, "Participant does not exist")
+        if (participant := Participant.find_by_ids(session, community_id, user.id)) is None:
+            return controller.abort(403, "Permission Denied: Participant not found")
         # Participant role check
         if participant.role == ParticipantRole.OWNER:
             deleted_news.deleted = True
             return {"a": "News was successfully deleted"}, 200
         else:
-            return controller.abort(403, "Must have the right of the OWNER")
+            return controller.abort(403, "Permission Denied: Low role")
