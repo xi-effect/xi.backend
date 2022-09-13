178b1f2e466531105618b6dca5a042b30c5cd16e
qzart45hi <qwert45hi@yandex.ru>
2022-09-14 01:03:06 +0300

    feat: update templates

--- templates/_db.py
+++ templates/_db.py
@@ -1,13 +1,6 @@
 from __future__ import annotations
 
-from datetime import datetime, timedelta
-from typing import Callable
-
-from sqlalchemy import Column, ForeignKey, select
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.sqltypes import Integer, String, Boolean, JSON, DateTime, Text, Enum
-
-from common import Identifiable, PydanticModel, TypeEnum, Base, User, sessionmaker, JSONWithModel
+from common import Base
 
 
 class TABLE(Base):
