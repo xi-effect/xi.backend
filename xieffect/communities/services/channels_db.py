51a75a719504f8d7a11d8316ff6c91c5dbb410b9
porebrikk <jam.d456@gmail.com>
2022-09-21 13:34:32 +1000

    feat: channel & channeltype models

--- xieffect/communities/services/channels_db.py
+++ xieffect/communities/services/channels_db.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 from sqlalchemy import Column, ForeignKey, select
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.sql.sqltypes import Integer, String, Text
+from sqlalchemy.sql.sqltypes import Integer, String, Text, Enum
 
-from common import Base, Identifiable, db, PydanticModel
+from common import Base, Identifiable, db, PydanticModel, TypeEnum
 
 MAX_CHANNELS: int = 50
 
@@ -18,7 +18,11 @@ class ChannelCategory(Base, Identifiable):
     description = Column(Text, nullable=True)
 
     # Previous category related
-    prev_category_id = Column(Integer, ForeignKey("channel_categories.id"))
+    prev_category_id = Column(
+        Integer,
+        ForeignKey("channel_categories.id"),
+        nullable=True,
+    )
     prev_category = relationship(
         "ChannelCategory",
         remote_side=[id],
@@ -26,7 +30,11 @@ class ChannelCategory(Base, Identifiable):
     )
 
     # Next category related
-    next_category_id = Column(Integer, ForeignKey("channel_categories.id"))
+    next_category_id = Column(
+        Integer,
+        ForeignKey("channel_categories.id"),
+        nullable=True,
+    )
     next_category = relationship(
         "ChannelCategory",
         remote_side=[id],
@@ -37,15 +45,12 @@ class ChannelCategory(Base, Identifiable):
     community_id = Column(Integer, ForeignKey("community.id"), nullable=False)
     community = relationship(
         "Community",
-        backref=backref("categories", cascade="all, delete, delete-orphan")
+        backref=backref("categories", cascade="all, delete, delete-orphan"),
     )
 
     BaseModel = PydanticModel.column_model(id)
     CreateModel = PydanticModel.column_model(name, description)
-    IndexModel = BaseModel.column_model(
-        prev_category_id,
-        next_category_id,
-    ).combine_with(CreateModel)
+    IndexModel = BaseModel.combine_with(CreateModel)
 
     @classmethod
     def create(
@@ -65,18 +70,10 @@ class ChannelCategory(Base, Identifiable):
         )
 
     @classmethod
-    def find_by_prev_id(cls, community_id, prev_id) -> ChannelCategory | None:
+    def find_by_next_id(cls, community_id: int, next_id: int | None) -> ChannelCategory | None:
         stmt = select(cls).filter_by(
             community_id=community_id,
-            prev_category_id=prev_id
-        )
-        return db.session.get_first(stmt)
-
-    @classmethod
-    def find_by_next_id(cls, community_id, next_id) -> ChannelCategory | None:
-        stmt = select(cls).filter_by(
-            community_id=community_id,
-            next_category_id=next_id
+            next_category_id=next_id,
         )
         return db.session.get_first(stmt)
 
@@ -84,36 +81,72 @@ class ChannelCategory(Base, Identifiable):
     def find_by_id(cls, entry_id: int) -> ChannelCategory | None:
         return db.session.get_first(select(cls).filter_by(id=entry_id))
 
-# class ChannelType(TypeEnum):
-#     NEWS = 1
-#     TASKS = 2
-#     CHAT = 3
-#     ROOM = 4
-#
-#
-# class Channel(Base, Identifiable):
-#     __tablename__ = "channels"
-#
-#     global MAX_CHANNELS
-#
-#     # Vital
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(100), nullable=False)
-#
-#     type = Column(Enum(ChannelType))
-#
-#     # Previous channel related
-#     prev_channel_id = Column(Integer, ForeignKey("channels.id"))
-#     prev_channel = relationship(
-#         "Channel",
-#         remote_side=[id],
-#         foreign_keys=[prev_channel_id]
-#     )
-#
-#     # Next channel related
-#     next_channel_id = Column(Integer, ForeignKey("channels.id"))
-#     next_channel = relationship(
-#         "Channel",
-#         remote_side=[id],
-#         foreign_keys=[next_channel_id]
-#     )
+
+class ChannelType(TypeEnum):
+    NEWS = 1
+    TASKS = 2
+    CHAT = 3
+    ROOM = 4
+
+
+class Channel(Base, Identifiable):
+    __tablename__ = "channels"
+
+    # Vital
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+
+    type = Column(Enum(ChannelType), nullable=False)
+
+    # Previous channel related
+    prev_channel_id = Column(Integer, ForeignKey("channels.id"))
+    prev_channel = relationship(
+        "Channel",
+        remote_side=[id],
+        foreign_keys=[prev_channel_id],
+    )
+
+    # Next channel related
+    next_channel_id = Column(Integer, ForeignKey("channels.id"))
+    next_channel = relationship(
+        "Channel",
+        remote_side=[id],
+        foreign_keys=[next_channel_id],
+    )
+
+    # Category-related
+    category_id = Column(
+        Integer,
+        ForeignKey("channel_categories.id"),
+        nullable=True,
+    )
+    category = relationship("ChannelCategory", backref=backref("channels"))
+
+    @classmethod
+    def create(
+        cls,
+        name: str,
+        type: ChannelType,
+        prev_channel_id: int | None,
+        next_channel_id: int | None,
+        category_id: int | None,
+    ) -> Channel:
+        return super().create(
+            name=name,
+            type=type,
+            prev_channel_id=prev_channel_id,
+            next_channel_id=next_channel_id,
+            category_id=category_id,
+        )
+
+    @classmethod
+    def find_by_next_id(cls, category_id: int | None, next_id: int | None) -> Channel | None:
+        stmt = select(cls).filter_by(
+            category_id=category_id,
+            next_channel_id=next_id,
+        )
+        return db.session.get_first(stmt)
+
+    @classmethod
+    def find_by_id(cls, entry_id: int) -> Channel | None:
+        return db.session.get_first(select(cls).filter_by(id=entry_id))
