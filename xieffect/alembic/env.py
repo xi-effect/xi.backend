763c46ba8a633720b3ed30659ee96f6cc07e7ebf
qzart45hi <qwert45hi@yandex.ru>
2022-10-27 12:30:10 +0300

    fix: migrations, env.py & tests

--- xieffect/alembic/env.py
+++ xieffect/alembic/env.py
@@ -4,7 +4,7 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from api import db_meta
+from api import db
 from common import db_url
 
 # this is the Alembic Config object, which provides
@@ -20,7 +20,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = db_meta
+target_metadata = db.metadata
 
 
 # other values from the config, defined by the needs of env.py,
