bff03bfea3261ad112cd5d0f266fceaf947a70b9
exllent <egorsolomin6@gmail.com>
2022-11-25 10:18:57 +0300

    feat: add method new_role in sio

--- xieffect/communities/base/role_db.py
+++ xieffect/communities/base/role_db.py
@@ -1,11 +1,16 @@
-from flask_fullstack import Identifiable, PydanticModel, TypeEnum
-from sqlalchemy import Column, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.sqltypes import Integer, String, Enum
+from __future__ import annotations
+
 from typing import TypeVar
 
-from common import Base
+from flask_fullstack import Identifiable, PydanticModel, TypeEnum
+from sqlalchemy import Column, ForeignKey, func, select
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import Integer, String, Enum
+
+from common import Base, db
 from communities.base import Community, Participant
+
+LimitingQuantityRoles: int = 50
 
 
 class PermissionTypes(TypeEnum):
@@ -13,6 +18,7 @@ class PermissionTypes(TypeEnum):
 
 
 t = TypeVar("t", bound="Role")
+p = TypeVar("p", bound="RolePermission")
 
 
 class Role(Base, Identifiable):
@@ -23,15 +29,38 @@ class Role(Base, Identifiable):
     id = Column(Integer, primary_key=True)
     name = Column(String(32), nullable=False)
     color = Column(String(6), nullable=True)
-    community_id = Column(Integer, ForeignKey(Community.id, ondelete="CASCADE"), primary_key=True)
+    community_id = Column(
+        Integer, ForeignKey(Community.id, ondelete="CASCADE"), primary_key=True
+    )
 
     BaseModel = PydanticModel.column_model(id)
     CreateModel = PydanticModel.column_model(name, color)
-    IndexModel = BaseModel.combine_with(CreateModel)
+    IndexModel = BaseModel.column_model(community_id).combine_with(CreateModel)
+
+    @classmethod
+    def create(
+        cls,
+        name: str,
+        color: str | None,
+        community_id: int,
+    ) -> Role:
+        return super().create(
+            name=name,
+            color=color,
+            community_id=community_id,
+        )
 
     @classmethod
     def get_all(cls: type[t]) -> list[t]:
-        return cls.query.all()
+        return db.session.execute(select(cls)).all()
+
+    @classmethod
+    def get_count(cls: type[t], community_id: int) -> int:
+        return (
+            db.session.execute(func.count())
+            .select_from(cls)
+            .where(cls.community_id == community_id)
+        )
 
 
 class RolePermission(Base):
@@ -40,7 +69,19 @@ class RolePermission(Base):
     role_id = Column(Integer, ForeignKey(Role.id), primary_key=True)
     permission_type = Column(Enum(PermissionTypes), primary_key=True)
 
-    role = relationship("Role", cascade="all, delete, delete-orphan", single_parent=True)
+    role = relationship(
+        "Role", cascade="all, delete, delete-orphan", single_parent=True
+    )
+
+    def create(
+        cls: type[p],
+        role_id,
+        permission_type,
+    ) -> p:
+        return super().create(
+            role_id=role_id,
+            permission_type=permission_type,
+        )
 
 
 class ParticipantRole(Base):
@@ -49,5 +90,9 @@ class ParticipantRole(Base):
     participant_id = Column(Integer, ForeignKey(Participant.id), primary_key=True)
     role_id = Column(Integer, ForeignKey(Role.id), primary_key=True)
 
-    participants = relationship("Participant", cascade="all, delete, delete-orphan", single_parent=True)
-    role = relationship("Role", cascade="all, delete, delete-orphan", single_parent=True)
+    participants = relationship(
+        "Participant", cascade="all, delete, delete-orphan", single_parent=True
+    )
+    role = relationship(
+        "Role", cascade="all, delete, delete-orphan", single_parent=True
+    )
--- xieffect/communities/base/role_sio.py
+++ xieffect/communities/base/role_sio.py
@@ -1,21 +1,25 @@
 from __future__ import annotations
 
-from flask_fullstack import EventSpace
+from flask_fullstack import EventSpace, DuplexEvent, TypeEnum as enum
 from flask_socketio import leave_room, join_room
+from pydantic import BaseModel
 
 from common import EventController
 from .meta_db import Community, ParticipantRole
 from ..utils import check_participant
+from .role_db import Role, RolePermission, LimitingQuantityRoles as limit
 
 controller = EventController()
 
 
 @controller.route()
 class RolesEventSpace(EventSpace):
-
     @classmethod
     def room_name(cls, community_id: int):
         return f"cs-roles-{community_id}"
+
+    class CommunityIdModel(BaseModel):
+        community_id: int
 
     @controller.argument_parser()
     @controller.force_ack()
@@ -28,6 +32,29 @@ class RolesEventSpace(EventSpace):
         leave_room(self.room_name(community.id))
 
     @controller.argument_parser()
+    @controller.mark_duplex(Role.IndexModel, use_event=True)
     @check_participant(controller, role=ParticipantRole.OWNER)
-    def new_role(self):
-        pass
+    @controller.marshal_ack(Role.IndexModel)
+    def new_role(
+        self,
+        event: DuplexEvent,
+        name: str,
+        color: str,
+        permissions: list,
+        community: Community,
+    ):
+        if quantity := Role.get_count(community.id) > limit:
+            controller.abort(
+                400, f"Quantity exceeded in category now{quantity} must be not more {limit}"
+            )
+        else:
+            role = Role.create(name=name, color=color, community_id=community.id)
+            [
+                RolePermission.create(
+                    role_id=role.id, permission_type=enum.from_string(permission)
+                )
+                for permission in permissions
+            ]
+            event.emit_convert(role, self.room_name(community.id))
+            return role
+
