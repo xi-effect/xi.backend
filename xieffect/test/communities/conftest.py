6f936b3bc552441a62b19c6a360c2a62cad6505e
exllent <egorsolomin6@gmail.com>
2022-11-26 18:12:46 +0300

    fix: test/commnuties, out fixture & role-sio create

---
+++ xieffect/test/communities/conftest.py
@@ -0,0 +1,12 @@
+from pytest import fixture
+
+from common.testing import SocketIOTestClient
+from .base.meta_test import assert_create_community
+
+COMMUNITY_DATA = {"name": "test"}
+
+@fixture
+def test_community(socketio_client: SocketIOTestClient) -> int:
+    # TODO place more globally (duplicate from invites_test)
+    # TODO use yield & delete the community after
+    return assert_create_community(socketio_client, COMMUNITY_DATA)
