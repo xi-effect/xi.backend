779ef9c39516ddd87dc4f5876fd00306350131a2
exllent <egorsolomin6@gmail.com>
2022-11-27 19:56:16 +0300

    feat: add sio update & delete

--- xieffect/test/communities/base/role_test.py
+++ xieffect/test/communities/base/role_test.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from flask_fullstack import dict_equal, check_code
 
 from common.testing import SocketIOTestClient
+from communities.base import RolePermission
+from common import db
 
 PERMISSIONS_LIST = ["create", "read", "update", "delete"]
 
@@ -15,9 +17,9 @@ def get_roles_list(client, community_id: int) -> list[dict]:
 
 
 def test_role_creation(
-    client,
-    socketio_client,
-    test_community,
+        client,
+        socketio_client,
+        test_community,
 ):
     # Create second owner & base clients
     socketio_client2 = SocketIOTestClient(client)
@@ -28,9 +30,12 @@ def test_role_creation(
     for user in (socketio_client, socketio_client2):
         user.assert_emit_success("open_roles", community_id_json)
 
-    role_data = dict(
-        community_id_json, name="test_role", color="black", permissions=PERMISSIONS_LIST
-    )
+    role_data = {
+        "name": "test_role",
+        "color": "black",
+        "permissions": PERMISSIONS_LIST,
+    }
+    role_data.update(community_id_json)
 
     result_data = socketio_client.assert_emit_ack("new_role", role_data)
     role_data.pop("permissions")
@@ -43,3 +48,16 @@ def test_role_creation(
         user.assert_emit_success("close_roles", community_id_json)
 
     print(get_roles_list(client, test_community))
+    print(db.session.execute(db.select(RolePermission)).scalars().all())
+    print(PERMISSIONS_LIST)
+    PERMISSIONS_LIST.pop(3)
+    PERMISSIONS_LIST.pop(2)
+    PERMISSIONS_LIST.pop(1)
+    print(PERMISSIONS_LIST)
+    role_data_for_update = {"name": "updated_test_role", "color": "green", "permissions": PERMISSIONS_LIST,
+                            "community_id": test_community, "role_id": 1}
+
+    result_data2 = socketio_client.assert_emit_ack("update_role", role_data_for_update)
+    print(result_data2)
+    print(result_data)
+    print(db.session.execute(db.select(RolePermission)).scalars().all())
