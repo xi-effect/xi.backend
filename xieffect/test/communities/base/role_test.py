24fe02c29971c60196ca7013cf9bbc0c08ce20d9
exllent <egorsolomin6@gmail.com>
2022-12-11 19:29:36 +0300

    fix: check_permissions & test update_role without permissions

--- xieffect/test/communities/base/role_test.py
+++ xieffect/test/communities/base/role_test.py
@@ -22,11 +22,11 @@ def test_roles(
     socketio_client2 = SocketIOTestClient(client)
 
     # Create valid & invalid permissions list
-    permissions_list: list = [p.to_string() for p in PermissionTypes]
-    incorrect_permissions: list = ["test"]
+    permissions: list[str] = PermissionTypes.get_all_field_names()
+    incorrect_permissions: list[str] = ["test"]
 
     role_data = {
-        "permissions": sorted(permissions_list),
+        "permissions": sorted(permissions),
         "name": "test_role",
         "color": "FFFF00",
     }
@@ -40,19 +40,18 @@ def test_roles(
     successful_role_data = dict(**role_data)
     role_data.update(community_id_json)
 
-    for i in range(1, LIMITING_QUANTITY_ROLES + 2):
-        if i == LIMITING_QUANTITY_ROLES + 1:
-            socketio_client.assert_emit_success(
-                event_name="new_role",
-                data=role_data,
-                code=400,
-                message="Quantity exceeded",
-            )
-        else:
-            successful_role_data["id"] = i
-            result_data = socketio_client.assert_emit_ack("new_role", role_data)
-            assert dict_equal(result_data, successful_role_data, *result_data.keys())
-            socketio_client2.assert_only_received("new_role", successful_role_data)
+    for i in range(1, LIMITING_QUANTITY_ROLES + 1):
+        successful_role_data["id"] = i
+        result_data = socketio_client.assert_emit_ack("new_role", role_data)
+        assert dict_equal(result_data, successful_role_data, *result_data.keys())
+        socketio_client2.assert_only_received("new_role", successful_role_data)
+
+    socketio_client.assert_emit_success(
+        event_name="new_role",
+        data=role_data,
+        code=400,
+        message="Quantity exceeded",
+    )
 
     # Assert 50 roles
     for index, data in enumerate(get_roles_list(client, test_community), 1):
@@ -93,31 +92,33 @@ def test_roles(
         roles_data_list
     )
 
+    # Assert role update with different data
     update_data = {
         "name": "update_test_name_role",
         "color": "00008B",
-        "permissions": sorted(permissions_list[1:]),
+        "permissions": sorted(permissions[1:]),
     }
 
-    data_for_update_role = dict(**update_data, **community_id_json, role_id=role_id)
+    update_role_data = dict(**update_data, **community_id_json, role_id=role_id)
+    successful_data = dict(**update_data, id=role_id)
+    second_update_role_data = update_role_data.copy()
+    second_update_role_data.pop("permissions")
+    second_update_role_data.pop("color")
+    update_data_list = [update_role_data, second_update_role_data]
 
-    successful_data_for_update_role = dict(**update_data, id=role_id)
+    for data in update_data_list:
+        result_data = socketio_client.assert_emit_ack("update_role", data)
+        assert dict_equal(
+            result_data,
+            successful_data,
+            *successful_data.keys(),
+        )
+        socketio_client2.assert_only_received("update_role", successful_data)
 
-    # Assert update role
-    result_data = socketio_client.assert_emit_ack("update_role", data_for_update_role)
-    assert dict_equal(
-        result_data,
-        successful_data_for_update_role,
-        *successful_data_for_update_role.keys(),
-    )
-    socketio_client2.assert_only_received(
-        "update_role", successful_data_for_update_role
-    )
-
-    data_for_update_role["permissions"] = incorrect_permissions
+    update_role_data["permissions"] = incorrect_permissions
     socketio_client.assert_emit_ack(
         "update_role",
-        data_for_update_role,
+        update_role_data,
         code=400,
         message="Incorrect permissions",
     )
