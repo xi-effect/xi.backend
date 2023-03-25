b12d224d0471aef7125c3046f73e747609783479
niqzart <qwert45hi@yandex.ru>
2023-03-25 18:20:50 +0300

    deploy: switch to gevent & update greenlet

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 alembic upgrade head
-sleep 10
-gunicorn --bind 0.0.0.0:5000 --worker-class eventlet --workers 1 wsgi:application --timeout 600 --log-level 'debug'
+sleep 1
+gunicorn --bind 0.0.0.0:5000 --worker-class gevent --workers 1 wsgi:application --timeout 600 --log-level 'debug'
