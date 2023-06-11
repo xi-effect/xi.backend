58d8912fb8983b15dd75093040358d5fd846b8f4
niqzart <qwert45hi@yandex.ru>
2023-06-11 12:29:57 +0300

    fix: avatar requires MANAGE_COMMUNITY permission

--- xieffect/communities/base/meta_rst.py
+++ xieffect/communities/base/meta_rst.py
@@ -6,7 +6,8 @@ from flask_restx import Resource
 from common import ResourceController
 from vault import File
 from .meta_db import Community, Participant
-from .utils import check_participant
+from .roles_db import PermissionType
+from .utils import check_participant, check_permission
 
 controller = ResourceController(
     "communities-meta",
@@ -32,14 +33,14 @@ class CommunityAvatar(Resource):
         type=int,
     )
 
+    @check_permission(controller, PermissionType.MANAGE_COMMUNITY)
     @controller.argument_parser(parser)
-    @check_participant(controller)  # TODO check permission
     @controller.database_searcher(File, input_field_name="avatar_id")
     @controller.a_response()
     def post(self, community: Community, file: File) -> None:
         community.avatar_id = file.id
 
-    @check_participant(controller)  # TODO check permission
+    @check_permission(controller, PermissionType.MANAGE_COMMUNITY)
     @controller.a_response()
     def delete(self, community: Community) -> None:
         community.avatar.delete()
@@ -50,7 +51,7 @@ class ParticipantSearcher(Resource):
     parser: RequestParser = counter_parser.copy()
     parser.add_argument("search", type=str, required=False)
 
-    @check_participant(controller, use_community=True)
+    @check_participant(controller)
     @controller.argument_parser(parser)
     @controller.lister(10, Participant.FullModel)
     def get(self, search: str | None, community: Community, start: int, finish: int):
